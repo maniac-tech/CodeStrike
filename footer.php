0c0c80b5de2863165d24aea09fe6888ca65c81c3
Abhishek Jain <ajj_2396@yahoo.com>
2016-11-20 22:29:13 +0530

    Altered footer

---
+++ footer.php
@@ -0,0 +1,54 @@
+			<footer id="contact" class="page-footer default_color scrollspy">
+				<div class="footer-copyright default_color">
+					<div class="container">
+						<b> <p>&copy; 2016 codestrike.in<p></b>
+					</div>
+				</div>
+				<div class="container">  
+					<div class="row">
+						<div class="col l6 s12">
+							<form class="col s12" action="contact.php" method="post">
+								<div class="row">
+								<h5 class="white-text">Contact Us:</h5>
+									<div class="input-field col s6">
+										<i class="mdi-action-account-circle prefix white-text"></i>
+										<label for="icon_prefix" class="white-text">CodeStrike</label>
+									</div>
+									<br><br>
+									<div class="input-field col s6">
+										<i class="mdi-communication-email prefix white-text"></i>
+										
+										<label for="icon_email" class="white-text">codestrikehq@gmail.com</label>
+									</div>
+									
+								</div>
+							</form>
+						</div>
+						<div class="col l3 s12">
+
+
+						</div>
+						<div class="col l3 s12">
+							<h5 class="white-text">Social</h5>
+							<ul>
+								
+								<li>
+									<a class="white-text" href="https://www.facebook.com/CodeStrikeHQ/?fref=ts">
+										<i class="small fa fa-facebook-square white-text"></i> Facebook
+									</a>
+								</li>
+								<li>
+									<a class="white-text" href="https://github.com/codestrike">
+										<i class="small fa fa-github-square white-text"></i> Github
+									</a>
+								</li>
+								<li>
+									<a class="white-text" href="#">
+										<i class="small fa fa-linkedin-square white-text"></i> Linkedin
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
