3f6d58801390eaa4f993a39a4c4aeb1c975a85fd
Abhishek Jain <ajj_2396@yahoo.com>
2016-12-15 23:50:10 +0530

    Created Arduino workshop page

---
+++ arduinoworkshop.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <script src="js/jquery.min.js"></script>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+   <meta name="theme-color" content="#2196F3">
+   <title>Arduino workshop by CodeStrike</title>
+
+   <!-- CSS  -->
+   <link href="min/plugin-min.css" type="text/css" rel="stylesheet">
+   <link href="min/custom-min.css" type="text/css" rel="stylesheet" >
+</head>
+<body id="top" class="scrollspy">
+
+    <!-- Pre Loader -->
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+
+    </div>
+
+    <!--Navigation-->
+    <div class="navbar-fixed">
+        <nav class="purple" role="navigation">
+            <div class="container">
+                <div class="nav-wrapper">
+                    <a href="index.php" id="logo-container" class="brand-logo">CODESTRIKE</a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="index.html">About us</a></li>
+                        <li><a href="index.html">work</a></li>
+                        <li><a href="#team">Team</a></li>
+                        <li><a href="#contact">Contact</a></li>
+
+                    </ul>
+                    <ul id="nav-mobile" class="side-nav">
+                        <li><a href="#intro">About us</a></li>
+                        <li><a href="#work">Work</a></li>
+                        <li><a href="#team">Team</a></li>
+                        <li><a href="#contact">Contact</a></li>
+
+                    </ul>
+                    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <!--team-->
+    <div class="section scrollspy" id="team">
+        <div class="container">
+            <h2 style="color:#A020F0">Arduino Workshop  </h2>
+            <div class="row">
+                <div >
+                    <div class="card card-avatar">
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">This Page is still under development.
+                                <br/>
+
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>                          
+
+    <!--Footer-->
+    <?php 
+        require 'footer.php';
+     ?>
+
+
+<!--  Scripts-->
+<script src="min/plugin-min.js"></script>
+<script src="min/custom-min.js"></script>
+
+
+
+</body>
+</html>
