ffa2f6f58ea2d79392f57ce7349bb0f82fb1fcb1
Abhishek Jain <ajj_2396@yahoo.com>
2016-12-19 17:44:39 +0530

    Added navigation bar

--- arduinoworkshop.php
+++ arduinoworkshop.php
@@ -24,30 +24,9 @@
     </div>
 
     <!--Navigation-->
-    <div class="navbar-fixed">
-        <nav class="purple" role="navigation">
-            <div class="container">
-                <div class="nav-wrapper">
-                    <a href="index.php" id="logo-container" class="brand-logo">CODESTRIKE</a>
-                    <ul class="right hide-on-med-and-down">
-                        <li><a href="index.html">About us</a></li>
-                        <li><a href="index.html">work</a></li>
-                        <li><a href="#team">Team</a></li>
-                        <li><a href="#contact">Contact</a></li>
-
-                    </ul>
-                    <ul id="nav-mobile" class="side-nav">
-                        <li><a href="#intro">About us</a></li>
-                        <li><a href="#work">Work</a></li>
-                        <li><a href="#team">Team</a></li>
-                        <li><a href="#contact">Contact</a></li>
-
-                    </ul>
-                    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
-                </div>
-            </div>
-        </nav>
-    </div>
+    <?php 
+        require 'navigationBar.php'; 
+    ?>
 
     <!--Event Details-->
     <div class="section scrollspy" id="team">
