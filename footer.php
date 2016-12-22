e40b96a745a07e0f3c355da8f9ef9ca7302dad6e
Abhishek Jain <ajj_2396@yahoo.com>
2016-12-22 22:52:29 +0530

    Added codechef logo with hyperlink to codechef's page

--- footer.php
+++ footer.php
@@ -1,54 +1,55 @@
-			<footer id="contact" class="page-footer default_color scrollspy">
-				<div class="footer-copyright default_color">
-					<div class="container">
-						<b> <p>&copy; 2016 codestrike.in<p></b>
-					</div>
-				</div>
-				<div class="container">  
+<footer id="contact" class="page-footer default_color scrollspy">
+	<div class="footer-copyright default_color">
+		<div class="container">
+			<b> <p>&copy; 2016 codestrike.in<p></b>
+		</div>
+	</div>
+	<div class="container">  
+		<div class="row">
+			<div class="col l4 s12">
+				<form class="col s12" action="contact.php" method="post">
 					<div class="row">
-						<div class="col l6 s12">
-							<form class="col s12" action="contact.php" method="post">
-								<div class="row">
-								<h5 class="white-text">Contact Us:</h5>
-									<div class="input-field col s6">
-										<i class="mdi-action-account-circle prefix white-text"></i>
-										<label for="icon_prefix" class="white-text">CodeStrike</label>
-									</div>
-									<br><br>
-									<div class="input-field col s6">
-										<i class="mdi-communication-email prefix white-text"></i>
-										
-										<label for="icon_email" class="white-text">codestrikehq@gmail.com</label>
-									</div>
-									
-								</div>
-							</form>
+						<h5 class="white-text">Contact Us:</h5>
+						<div class="input-field col s6">
+							<i class="mdi-action-account-circle prefix white-text"></i>
+							<label for="icon_prefix" class="white-text">CodeStrike</label>
 						</div>
-						<div class="col l3 s12">
+						<br><br>
+						<div class="input-field col s6">
+							<i class="mdi-communication-email prefix white-text"></i>
 
+							<label for="icon_email" class="white-text">codestrikehq@gmail.com</label>
+						</div>
 
-						</div>
-						<div class="col l3 s12">
-							<h5 class="white-text">Social</h5>
-							<ul>
-								
-								<li>
-									<a class="white-text" href="https://www.facebook.com/CodeStrikeHQ/?fref=ts">
-										<i class="small fa fa-facebook-square white-text"></i> Facebook
-									</a>
-								</li>
-								<li>
-									<a class="white-text" href="https://github.com/codestrike">
-										<i class="small fa fa-github-square white-text"></i> Github
-									</a>
-								</li>
-								<li>
-									<a class="white-text" href="#">
-										<i class="small fa fa-linkedin-square white-text"></i> Linkedin
-									</a>
-								</li>
-							</ul>
-						</div>
 					</div>
-				</div>
-			</footer>
+				</form>
+			</div>
+			<div class="col l4 s12">
+				<h5 class="white-text">Social</h5>
+				<ul>
+					
+					<li>
+						<a class="white-text" href="https://www.facebook.com/CodeStrikeHQ/?fref=ts">
+							<i class="small fa fa-facebook-square white-text"></i> Facebook
+						</a>
+					</li>
+					<li>
+						<a class="white-text" href="https://github.com/codestrike">
+							<i class="small fa fa-github-square white-text"></i> Github
+						</a>
+					</li>
+					<li>
+						<a class="white-text" href="#">
+							<i class="small fa fa-linkedin-square white-text"></i> Linkedin
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col l4 s12">
+				<a href="http://www.codechef.com" title="">
+					<img src="codechef logo.png" alt="CodeChef" style="width: 70%;vertical-align: top;">
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
