3e4ec40a83d63b67fb20af55a764f4249fa74336
Abhishek Jain <ajj_2396@yahoo.com>
2017-01-11 01:31:38 +0530

    Corrected the faults causing build log error on hosting

--- navigationBar.php
+++ navigationBar.php
@@ -4,17 +4,10 @@
                 <div class="nav-wrapper">
                     <a href="index.php" id="logo-container" class="brand-logo">CODESTRIKE</a>
                     <ul class="right hide-on-med-and-down">
-<<<<<<< HEAD
                         <li><a href="index.php">About us</a></li>
                         <li><a href="index.php">Events</a></li>
                         <li><a href="team.php">Team</a></li>
                         <li><a href="index.php">Contact</a></li>
-=======
-                        <li><a href="index.php #intro">About us</a></li>
-                        <li><a href="index.php #events">Events</a></li>
-                        <li><a href="#team">Team</a></li>
-                        <li><a href="#contact">Contact</a></li>
->>>>>>> navigationBar
 
                     </ul>
                     <ul id="nav-mobile" class="side-nav">
