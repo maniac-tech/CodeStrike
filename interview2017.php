5e6a7bba4d4af2c3a3075561fecf6b7b49688a59
Abhishek Jain <ajj_2396@yahoo.com>
2017-01-30 23:20:10 +0530

    Added interview 2017

---
+++ interview2017.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <script src="js/jquery.min.js"></script>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+   <meta name="theme-color" content="#2196F3">
+   <title>Interview 2017</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.png" /> <!--Tab Icon-->
+
+   <!-- CSS  -->
+   <link href="min/plugin-min.css" type="text/css" rel="stylesheet">
+   <link href="min/custom-min.css" type="text/css" rel="stylesheet" >
+</head>
+<body id="top" class="scrollspy">
+
+    <!-- Pre Loader -->
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+
+    </div>
+
+    <!--Navigation-->
+    <?php 
+        require 'navigationBar.php'; 
+    ?>
+
+
+    <!--Event Details-->
+    <div class="section scrollspy" id="team">
+        <div class="container">
+            <h2 style="color:#A020F0">Interview 2017</h2>
+            <div class="row">
+                <div >
+                    <div class="card card-avatar">
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">This Page is still under development.
+                                <br/>
+
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>                          
+
+    <!--Footer-->
+    <?php 
+        require 'footer.php';
+     ?>
+
+
+<!--  Scripts-->
+<script src="min/plugin-min.js"></script>
+<script src="min/custom-min.js"></script>
+
+
+
+</body>
+</html>
