458d6f62535e5bb517d4105ce1e653ebe9ad816d
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-01 23:52:44 +0530

    Added countdown timer

--- interview2017.php
+++ interview2017.php
@@ -6,11 +6,14 @@
    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
    <meta name="theme-color" content="#2196F3">
    <title>Interview 2017</title>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.png" /> <!--Tab Icon-->
+   <link rel="shortcut icon" type="image/x-icon" href="favicon.png" /> <!--Tab Icon-->
 
    <!-- CSS  -->
    <link href="min/plugin-min.css" type="text/css" rel="stylesheet">
    <link href="min/custom-min.css" type="text/css" rel="stylesheet" >
+
+   <!-- Timer -->
+   <link rel="stylesheet" href="styleTimer.css"> 
 </head>
 <body id="top" class="scrollspy">
 
@@ -25,33 +28,49 @@
 
     <!--Navigation-->
     <?php 
-        require 'navigationBar.php'; 
+    require 'navigationBar.php'; 
     ?>
 
 
     <!--Event Details-->
     <div class="section scrollspy" id="team">
         <div class="container">
-            <h2 style="color:#A020F0">Interview 2017</h2>
-            <div class="row">
-                <div >
-                    <div class="card card-avatar">
-                        <div class="card-content">
-                            <span class="card-title activator grey-text text-darken-4">This Page is still under development.
-                                <br/>
-
-                            </span>
-                        </div>
-                    </div>
-                </div>
+            <div id="regTimer">
+                
+            
+            <h1>Time left for Registrations</h1>
+            <div id="clockdiv">
+              <div>
+                <span class="days"></span>
+                <div class="smalltext">Days</div>
             </div>
-        </div>
-    </div>                          
+            <div>
+                <span class="hours"></span>
+                <div class="smalltext">Hours</div>
+            </div>
+            <div>
+                <span class="minutes"></span>
+                <div class="smalltext">Minutes</div>
+            </div>
+            <div>
+                <span class="seconds"></span>
+                <div class="smalltext">Seconds</div>
+            </div>
+            </div>
+            </div>
+        <script type="text/javascript" src="app.js"></script>
 
-    <!--Footer-->
-    <?php 
-        require 'footer.php';
-     ?>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>                          
+
+<!--Footer-->
+<?php 
+require 'footer.php';
+?>
 
 
 <!--  Scripts-->
