3e7965c746bfa1b2d3a538f84582e18e4b24fa6c
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-07 21:24:03 +0530

    Added navigation bar to base file

--- home.php
+++ home.php
@@ -25,9 +25,33 @@
 	</div>
 
 	<!--Navigation-->
-    <?php 
-        require 'navigationBar.php'; 
-    ?>
+    <div class="navbar-fixed">
+        <nav class="purple" role="navigation">
+            <div class="container">
+                <div class="nav-wrapper">
+                    <a href="index.php" id="logo-container" class="brand-logo">CODESTRIKE</a>
+
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="#intro">About us</a></li>
+                        <li><a href="#work">Events</a></li>
+                        <li><a href="#team">Team</a></li>
+                        <li><a href="#contact">Contact</a></li>
+
+                    </ul>
+
+                    <ul id="nav-mobile" class="side-nav">
+                        <li><a href="#intro">About us</a></li>
+                        <li><a href="#work">Events</a></li>
+                        <li><a href="#team">Team</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+
+                    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+                </div>
+            </div>
+        </nav>
+    </div>
+
 
 	<!--Hero-->
 	<div class="section no-pad-bot" id="index-banner">
