ea3a32e140c293fed676a381fb600e315e786312
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-11 22:15:55 +0530

    Updated acc to new code

--- admin login/validate.php
+++ admin login/validate.php
@@ -1,11 +1,51 @@
-<?php 
+<?php
+	//include database config file 
+	include('connect.php');
+	session_start();
+	$id="0";
+	$console='';
+	$session_id='';
+
 	if ($_SERVER["REQUEST_METHOD"] == "POST"){
-		$name=$_POST["loginID"];
+		$id=$_POST["emailID"];
 		$password=$_POST["password"];
 
-		if ($name="admin" && $password=="admin")
-			header("location:https://www.google.com");
-		else
-			header("location:https://www.yahoo.com");
+		//create query
+		$query="SELECT * FROM login WHERE id='$id' AND password='$password'";
+		$result=$conn->query($query);
+
+		//check the query in the database
+		if ($result){
+			if ($result->num_rows > 0){	
+				//Create session id and store them 
+				session_regenerate_id();
+				$user=$result->fetch_assoc();
+
+				$_SESSION['SESS_MEMBER_ID']=$user['id'];
+				
+				$session_id=$_SESSION['SESS_MEMBER_ID'];
+			}
+			else
+				$console="Failed";
+
+			echo $console;
+		}
 	}
  ?>
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+</head>
+<body>
+	<p>
+		Your username is <?php 
+			echo $session_id;
+			?>	
+		<br>
+		<a href="anotherpage.php">Move to another page</a>	
+		<br>
+		Click here to <a href="logout.php">logout</a>	
+	</p>
+</body>
+</html>
