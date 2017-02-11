97b131bb55489b0b86d0b22d6c8420630edcf28b
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-12 00:10:07 +0530

    Db connectivity issues solved

--- admin login/validate.php
+++ admin login/validate.php
@@ -8,7 +8,7 @@
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST"){
 		$id=$_POST["loginID"];
-		$password=$_POST["password"];
+		$password=$_POST["loginPassword"];
 
 		echo $id."<br>";
 		echo $password."<br>";
