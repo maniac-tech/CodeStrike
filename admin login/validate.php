214e18dfca5830f99d40dae2c6ebcb61d7dad070
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-12 17:35:10 +0530

    Added the alpha interview design

--- admin login/validate.php
+++ admin login/validate.php
@@ -1,10 +1,45 @@
 <?php
 	//include database config file 
-	include('connect.php');
-	session_start();
+include('connect.php');
+session_start();
+
+$id="0";
+$console='';
+$session_id='';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+	$id=$_POST["loginID"];
+	$password=$_POST["loginPassword"];
+
+	echo $_POST['emailID']."<br>";
+	echo "ID:".$id."<br>";
+	echo "Password:".$password."<br>";
+
+			//create query
+	$query="SELECT * FROM login WHERE id='$id' AND password='$password'";
+	$result=$conn->query($query);
+
+			//check the query in the database
+	if ($result){
+		if ($result->num_rows > 0){	
+					//Create session id and store them 
+			session_regenerate_id();
+			$user=$result->fetch_assoc();
+
+			$_SESSION['SESS_MEMBER_ID']=$user['id'];
+			
+			$session_id=$_SESSION['SESS_MEMBER_ID'];
+			$console="DB query: Successful"
+		}
+		else
+			$console="Failed";
+
+		echo $console;
+	}
+}
 ?>
-	
- <!DOCTYPE html>
+
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Test</title>
@@ -12,45 +47,13 @@
 <body>
 	<p><h1>After-Login Page</h1></p>
 	<p>Below is a detailed suammary of the connection.</p>
-	<?php 
-		$id="0";
-		$console='';
-		$session_id='';
-
-		if ($_SERVER["REQUEST_METHOD"] == "POST"){
-			$id=$_POST["loginID"];
-			$password=$_POST["loginPassword"];
-
-			echo $_POST['emailID']."<br>";
-			echo "ID:".$id."<br>";
-			echo "Password:".$password."<br>";
-
-			//create query
-			$query="SELECT * FROM login WHERE id='$id' AND password='$password'";
-			$result=$conn->query($query);
-
-			//check the query in the database
-			if ($result){
-				if ($result->num_rows > 0){	
-					//Create session id and store them 
-					session_regenerate_id();
-					$user=$result->fetch_assoc();
-
-					$_SESSION['SESS_MEMBER_ID']=$user['id'];
-					
-					$session_id=$_SESSION['SESS_MEMBER_ID'];
-					$console="DB query: Successful"
-				}
-				else
-					$console="Failed";
-
-				echo $console;
-			}
-		}
-	 ?>
+	
 	<p>
 		Your Credentials:<br>
 		Your username is <?php echo $session_id;?>	
+
+		<hr>
+		The interview page will be shown here after successful implementation.
 		<br>
 		<a href="anotherpage.php">Move to another page</a>	
 		<br>
