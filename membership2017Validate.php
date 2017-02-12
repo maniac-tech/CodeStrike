ae6f766dc2181b7852cb2b949d3fad4f7f5c0585
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-12 22:15:57 +0530

    Membership 2017 files added

---
+++ membership2017Validate.php
@@ -0,0 +1,21 @@
+<?php 
+	require_once('membership2017Connect.php');
+ 	
+ 	$regStatus="";
+
+ 	if ($_SERVER["REQUEST_METHOD"]=="POST"){
+ 		$name=$_POST["fullName"];
+ 		$emailID=$_POST["emailID"];
+ 		$mobileNo=$_POST["mobileNo"];
+
+ 		$query="INSERT INTO MEMBERS2017 (Name,EmailID,MobileNo) VALUES ('$name','$emailID','$mobileNo')";
+
+ 		$result=$conn->query($query);
+
+ 		if($result){
+ 			header("location:RegComplete.html");
+ 		}
+ 		else
+ 			header("location:RegFailed.html");
+ 	}
+ ?>
