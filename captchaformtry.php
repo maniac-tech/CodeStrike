37ea3949d1e3f288ce085363dd0d90da9edb984b
Tejas-Nanaware <Tejas-Nanaware@users.noreply.github.com>
2017-02-14 00:56:07 +0530

    testing captcha

---
+++ captchaformtry.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SUP</title>
+</head>
+<body>
+Hey this is HTML
+<form>
+<p><img src="captcha.php" width="120" height="30" border="1" alt="CAPTCHA"></p>
+          <p><input type="text" size="6" maxlength="5" name="captcha" value=""><br>
+          	<small>copy the digits from the image into this box</small></p>
+</form>
+</body>
+</html>
