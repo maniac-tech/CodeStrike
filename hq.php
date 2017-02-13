ecbd7f3cc2286787306976f8eb5b5f653735bfda
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-14 00:17:15 +0530

    Redirected

--- hq.php
+++ hq.php
@@ -11,7 +11,7 @@
       <input type="text" placeholder="username" name="loginID"/>
       <input type="password" placeholder="password" name="loginPassword"/>
       <input type="Submit" value="Login">
-      <p class="message">Not registered? <a href="interview.html">Create an account</a></p>
+      <p class="message">Not registered? <a href="membership2017.php">Create an account</a></p>
     </form>
   </div>
 </div>
