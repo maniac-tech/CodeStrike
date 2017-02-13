686c8bd96bdc5ec1bec6472898da3faeb46258fa
Tejas-Nanaware <Tejas-Nanaware@users.noreply.github.com>
2017-02-14 00:28:38 +0530

    Added Captcha Validation

--- interview2017Validate.php
+++ interview2017Validate.php
@@ -2,6 +2,9 @@
 	require_once('interview2017Connect.php');
  	
  	$regStatus="";
+ 	session_start();
+    if($_POST['captcha'] != $_SESSION['digit']) die("Sorry, the CAPTCHA code entered was incorrect!");
+    session_destroy();
 
  	if ($_SERVER["REQUEST_METHOD"]=="POST"){
  		$name=$_POST["name"];
@@ -12,7 +15,7 @@
  		$uid=uniqid('',true);
  		echo $uid;
 
- 		$query="INSERT INTO  Interview2017(Name,EmailID,MobileNo,Branch,Year,UniqueID,Status) VALUES ('$name','$emailID','$mobileNo','$branch','$year','$uid','1')";
+ 		$query="INSERT INTO  Interview2017(Name,EmailID,MobileNo,Branch,Year,UniqueID) VALUES ('$name','$emailID','$mobileNo','$branch','$year','$uid')";
 
  		$result=$conn->query($query);
 
