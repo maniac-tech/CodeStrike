7638674c739199fd632b3d884b0fcea13e72d2ee
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-14 02:58:41 -0800

    Update interview2017Validate.php
    Added echo for debugging

--- interview2017Validate.php
+++ interview2017Validate.php
@@ -1,45 +1,48 @@
+
 <?php 
-	require_once('interview2017Connect.php');
- 	
- 	$regStatus="";
- 	session_start();
+
+require_once('interview2017Connect.php');
 
 
 
- 	$captcha=$_POST['g-recaptcha-response'];
- 	$secretKey = "6LeshBUUAAAAAC7SiwFP0M1cW-OfVqxXHza0rXmc";
-    $request = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$recaptchaResponse}&remoteip={$userIP}");
+$regStatus="";
 
-    if(!strstr($request, "true")){
-       
-        session_destroy();
-    }
-    else{
-        
-    
+session_start();
+$captcha=$_POST['g-recaptcha-response'];
+$secretKey = "6LeshBUUAAAAAC7SiwFP0M1cW-OfVqxXHza0rXmc";
+$request = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$recaptchaResponse}&remoteip={$userIP}");
 
+echo "Before captcha if loop";
+
+if(!strstr($request, "true")){
+	session_destroy();
+}
+
+else{
+	echo "Entered echo session in captcha else";
     //if($_POST['captcha'] != $_SESSION['digit']) die("Sorry, the CAPTCHA code entered was incorrect!");
+
     //session_destroy();
 
- 	if ($_SERVER["REQUEST_METHOD"]=="POST"){
- 		$name=$_POST["name"];
- 		$emailID=$_POST["email"];
- 		$mobileNo=$_POST["phone"];
- 		$branch=$_POST["stream"];
- 		$year=$_POST["year"];
- 		$uid=uniqid('',true);
- 		echo $uid;
+	if ($_SERVER["REQUEST_METHOD"]=="POST"){
 
- 		$query="INSERT INTO  Interview2017(Name,EmailID,MobileNo,Branch,Year,UniqueID) VALUES ('$name','$emailID','$mobileNo','$branch','$year','$uid')";
+		$name=$_POST["name"];
+		$emailID=$_POST["email"];
+		$mobileNo=$_POST["phone"];
+		$branch=$_POST["stream"];
+		$year=$_POST["year"];
+		$uid=uniqid('',true);
+		echo $uid;
+		$query="INSERT INTO  Interview2017(Name,EmailID,MobileNo,Branch,Year,UniqueID) VALUES ('$name','$emailID','$mobileNo','$branch','$year','$uid')";
+		$result=$conn->query($query);
 
- 		$result=$conn->query($query);
+		if($result){
+			header("location:RegComplete.html");
+		}
+		else{
+			header("location:RegFailed.html");
+		}
+	}
+}
 
- 		if($result){
- 			header("location:RegComplete.html");
- 		}
- 		else{
- 			header("location:RegFailed.html");
- 		}
- 	}
-    }
- ?>
+?>
