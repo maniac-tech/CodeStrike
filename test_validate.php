7f62a05cffa7a1ff4c96f9eda2328b944e03375d
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-15 12:56:14 +0530

    Debug

--- test_validate.php
+++ test_validate.php
@@ -1,36 +1,5 @@
 <?php
 	//include database config file 
 	echo "test_validate: begun <br>";
-include('test_connect.php');
-require_once('interview2017Connect.php');
 
-session_start();
-
-$id="0";
-$console='';
-$session_id='';
-
-echo "test_validate before if loop <br>";
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-	$id=$_POST["loginID"];
-	$password=$_POST["loginPassword"];
-
-	//create query
-	$query="SELECT * FROM Members2017 WHERE EmailID='$id' AND MobileNo='$password'";
-	$result=$conn->query($query);
-
-			//check the query in the database
-	if ($result){
-		if ($result->num_rows > 0){	
-					//Create session id and store them 
-			session_regenerate_id();
-			$user=$result->fetch_assoc();
-
-			$_SESSION['SESS_MEMBER_ID']=$user['id'];
-			
-			$session_id=$_SESSION['SESS_MEMBER_ID'];
-			header('location:questionspage.php');
-		}
-	}
-}
 ?>
