95d1c774bc26f58e3ce8d243a8a965c6de855103
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-15 12:10:22 +0530

    Added questions panel

---
+++ test_connect.php
@@ -0,0 +1,16 @@
+<?php 
+	$servername=getenv('DATABASE_SERVER_NAME');
+	$databaseName=getenv('DATABASE_NAME');
+	$tableName=getenv('DATABASE_TABLE_1');
+	$username=getenv('DATABASE_USERNAME');
+	$password=getenv('DATABASE_PASSWORD');
+
+	//create connection
+	$conn=mysqli_connect($servername,$username,$password,$databaseName);
+
+	//check connection
+	if (!$conn){
+		die('Connection failed:'.mysqli_connect_error());
+	}
+	
+ ?>
