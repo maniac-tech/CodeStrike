8c7f9ba72b8cfb61be8a9a09c5d44bc43648346a
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-16 01:00:44 +0530

    Update

--- test_submit.php
+++ test_submit.php
@@ -3,21 +3,21 @@
 	require('test_connect.php');
 	
 	// Question1
-	echo "Question is : <br>".$_SESSION["q1"]."<br>";
+	echo "Question is : ".$_SESSION["q1"]."<br>";
 	echo "Answer is ".$_POST["option1"]."<br>";
 
 	//Question2
-	echo "Question is : <br>".$_SESSION["q2"]."<br>";
+	echo "Question is : ".$_SESSION["q2"]."<br>";
 	echo "Answer is ".$_POST["option2"]."<br>";
 	
 	//Question3
-	echo "Question is : <br>".$_SESSION["q3"]."<br>";
+	echo "Question is : ".$_SESSION["q3"]."<br>";
 	echo "Answer is ".$_POST["group1"]."<br>";
 
 	//Question4
-	echo "Question is : <br>".$_SESSION["q4"]."<br>";
+	echo "Question is : ".$_SESSION["q4"]."<br>";
 	echo "Answer is ".$_POST["answer1"]."<br>";
 	//Question5
-	echo "Question is : <br>".$_SESSION["q5"]."<br>";
+	echo "Question is : ".$_SESSION["q5"]."<br>";
 	echo "Answer is ".$_POST["answer2"]."<br>";
  ?>
