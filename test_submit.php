0364b077cd14178786a6e6c552d9141c327ab944
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-16 01:51:19 +0530

    Update

--- test_submit.php
+++ test_submit.php
@@ -12,14 +12,14 @@
 	
 	//Question3
 	echo "Question is : ".$_SESSION["q3"]."<br>";
-	echo "Answer is ".$_POST["option3"]."<br>";
+	echo "Answer is ".$_POST["group1"]."<br>";
 
 	//Question4
 	echo "Question is : ".$_SESSION["q4"]."<br>";
-	echo "Answer is ".$_POST["option4"]."<br>";
+	echo "Answer is ".$_POST["answer1"]."<br>";
 	//Question5
 	echo "Question is : ".$_SESSION["q5"]."<br>";
-	echo "Answer is ".$_POST["option5"]."<br>";
+	echo "Answer is ".$_POST["answer2"]."<br>";
 
 	// $query = "INSERT INTO Results2017 (Name, Year, Stream, Q1, A1, Q3, A3, Q4, A4, Q5, A5) VALUES ()";
  ?>
