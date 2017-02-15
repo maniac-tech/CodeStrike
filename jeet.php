b095de55984904c1e820b6a8f29d0c097d8930ae
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-15 22:15:43 +0530

    Added php code

--- jeet.php
+++ jeet.php
@@ -58,8 +58,82 @@
   <dl>
     <?php
     session_start();
-    $question = $_SESSION['question'];
-    $answer = $_SESSION['answer'];
+    // Below two lines have been commented out, as they were found illogical:
+    // $question = $_SESSION['question'];
+    // $answer = $_SESSION['answer'];
+
+    //Below code has been copied from Burhan's snippet: 
+    require_once 'connect.php';
+    $db = new connect();
+    $conn = $db->connection();
+    session_start();
+    $array =  array('0' => 0);
+    $question = array();
+    $sql = " Select uid From questions";
+    $result = mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result) > 0 )
+    {
+     while ($row = mysqli_fetch_assoc($result)) 
+     {
+       $uid[] = $row["uid"];
+     }
+    }
+   for ($i=1; $i <=3; $i++) { 
+      $num = rand( 1, 14 );
+      if ($uid[$num-1] == 0) {
+       if (check($array, $num)) {
+        $i--;
+        continue;
+      } 
+
+      $query = mysqli_query($conn,"SELECT * FROM questions WHERE id = '$num'");
+      $row = mysqli_fetch_assoc($query);
+      $question[$i] = $row["question"];
+      $array[$i] = $num;
+    }
+
+    }
+    for ($i=4; $i <=5; $i++) { 
+        $num = rand( 16, 24 );
+        if ($uid[$num-1] == 1) {
+         if (check($array, $num)) {
+          $i--;
+          continue;
+        } 
+
+        $query = mysqli_query($conn,"SELECT * FROM questions WHERE id = '$num'");
+        $row = mysqli_fetch_assoc($query);
+        $question[$i] = $row["question"];
+        $array[$i] = $num;
+      }
+
+      }
+    $k = 1;
+    for ($j=1; $j <4 ; $j++) { 
+      $num = $array[$j]*4;
+      $limit = $num - 4;
+      while ($num>$limit) { 
+       $query = mysqli_query($conn,"SELECT * FROM answers WHERE id = '$num'");
+       $row = mysqli_fetch_assoc($query);
+       $answer[$k] = $row["answers"];
+       $num--;
+       $k++;
+     }
+    }
+
+    mysqli_close($conn);
+    $_SESSION ['question'] = $question;
+    function check($array, $num)
+    {
+      for ($i=0; $i <count($array); $i++) { 
+       if($array[$i] == $num)
+       {
+        return true;
+      } 
+    }
+
+    }
+
     ?>
   </dl>
   
