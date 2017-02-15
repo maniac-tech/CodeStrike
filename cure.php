395486bd69f40d4a6fdb3ba4399bba3ea8cf80b9
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-16 02:44:56 +0530

    Cure V2.0

--- cure.php
+++ cure.php
@@ -1,6 +1,6 @@
 <?php  
 	require ('interview2017Connect.php');
-	$query = "DELETE FROM `Interview2017` WHERE `Interview2017`.`Name` = 'fjqotuko' AND `Interview2017`.`EmailID` = '\r\n SomeCustomInjectedHeader:injected_by_wvs' AND `Interview2017`.`MobileNo` = '555-666-06' AND `Interview2017`.`Branch` = '1' AND `Interview2017`.`Year` = '1967' AND `Interview2017`.`DOB` = '0000-00-00' AND `Interview2017`.`UniqueID` = '58a1e73530cce5.75904258' AND `Interview2017`.`Status` = 3 AND `Interview2017`.`Timestamp` = '0000-00-00 00:00:00' LIMIT 1";
+	$query = "DELETE FROM `Interview2017` WHERE `Interview2017`.`Name` = 'hrncdxjo' AND `Interview2017`.`EmailID` = 'sample@email.tst' AND `Interview2017`.`MobileNo` = '555-666-06' AND `Interview2017`.`Branch` = '1' AND `Interview2017`.`Year` = '/etc/passwd' AND `Interview2017`.`DOB` = '0000-00-00' AND `Interview2017`.`UniqueID` = '58a330dc8ac1b5.60816625' AND `Interview2017`.`Status` = 3 AND `Interview2017`.`Timestamp` = '2017-02-14 16:37:10' LIMIT 1";
 
 	$result = $conn->query($query);
 	if ($result){
