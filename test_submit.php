7d599efa13fd5a17ab4f3b826fb23a7f2ee38f7b
burhanuddinrampurawala <burhanuddinzrampurawala@gamil.com>
2017-02-16 10:27:45 +0530

    bug fixes-2

--- test_submit.php
+++ test_submit.php
@@ -2,24 +2,24 @@
 	session_start(); 
 	require('test_connect.php');
 	
-	// Question1
-	echo "Question is : ".$_SESSION["q1"]."<br>";
-	echo "Answer is ".$_POST["option1"]."<br>";
+	// // Question1
+	// echo "Question is : ".$_SESSION["q1"]."<br>";
+	// echo "Answer is ".$_POST["option1"]."<br>";
 
-	//Question2
-	echo "Question is : ".$_SESSION["q2"]."<br>";
-	echo "Answer is ".$_POST["option2"]."<br>";
+	// //Question2
+	// echo "Question is : ".$_SESSION["q2"]."<br>";
+	// echo "Answer is ".$_POST["option2"]."<br>";
 	
-	//Question3
-	echo "Question is : ".$_SESSION["q3"]."<br>";
-	echo "Answer is ".$_POST["group1"]."<br>";
+	// //Question3
+	// echo "Question is : ".$_SESSION["q3"]."<br>";
+	// echo "Answer is ".$_POST["group1"]."<br>";
 
-	//Question4
-	echo "Question is : ".$_SESSION["q4"]."<br>";
-	echo "Answer is ".$_POST["answer1"]."<br>";
-	//Question5
-	echo "Question is : ".$_SESSION["q5"]."<br>";
-	echo "Answer is ".$_POST["answer2"]."<br>";
+	// //Question4
+	// echo "Question is : ".$_SESSION["q4"]."<br>";
+	// echo "Answer is ".$_POST["answer1"]."<br>";
+	// //Question5
+	// echo "Question is : ".$_SESSION["q5"]."<br>";
+	// echo "Answer is ".$_POST["answer2"]."<br>";
 
 	$rq1=$_SESSION["q1"];
 	$rq2=$_SESSION["q2"];
@@ -32,7 +32,8 @@
 	$ra4=$_POST["answer1"];
 	$ra5=$_POST["answer2"];
 
-	$query = "INSERT INTO Results2017 (Q1, A1, Q2, A2,Q3, A3, Q4, A4, Q5, A5) VALUES (\"'$rq1'\",\"'$rq2'\",\"'$rq3'\",\"'$rq4'\",\"'$rq5'\",\"'$ra1',\"'$ra2',\"'$ra3'\",\"'$ra4'\",\"'$ra5'\")";
+	$query = "INSERT INTO Results2017 (Q1, A1, Q2, A2,Q3, A3, Q4, A4, Q5, A5) VALUES (\"'$rq1'\",\"'$ra1'\",\"'$rq2'\",\"'$ra2'\",\"'$rq3'\",\"'$ra3',\"'$rq4',\"'$ra4'\",\"'$rq5'\",\"'$ra5'\")";
+	echo $query;
 
 	$result=$conn->query($query);
 	if ($result){
