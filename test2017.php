c56164f1c6da87a545afcd5d8284033461e7f5d0
burhanuddin rampurawala <burhanuddinzrampurawala@gmail.com>
2017-02-16 13:17:20 +0530

    Update test2017.php

--- test2017.php
+++ test2017.php
@@ -8,8 +8,8 @@
 <div class="login-page">
   <div class="form">
     <form class="login-form" method="post" action="test_validate.php">
-      <input type="text" placeholder="username" name="loginID"/>
-      <input type="password" placeholder="password" name="loginPassword"/>
+      <input type="text" placeholder="Email-ID" name="loginID"/>
+      <input type="password" placeholder="phone number" name="loginPassword"/>
       <input type="Submit" value="Login">
       <p class="message">Not registered? <a href="membership2017.php">Create an account</a></p>
     </form>
