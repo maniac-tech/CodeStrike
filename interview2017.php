ea9efe72c4285117ac1cdf86f87b3fc83fa8f031
Abhishek Jain <ajj_2396@yahoo.com>
2017-02-21 18:48:17 +0530

    Results added

--- interview2017.php
+++ interview2017.php
@@ -1,68 +1,175 @@
-<!DOCTYPE html>
-<html>
-<head>
- <script src="js/jquery.min.js"></script>
- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
- <meta name="theme-color" content="#2196F3">
- <title>Interview 2017</title>
- <link rel="shortcut icon" type="image/x-icon" href="favicon.png" /> <!--Tab Icon-->
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<script src="js/jquery.min.js"></script>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+		<meta name="theme-color" content="#2196F3">
+		<title>Interview 2017</title>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.png" /> <!--Tab Icon-->
 
- <!-- CSS  -->
- <link href="min/plugin-min.css" type="text/css" rel="stylesheet">
- <link href="min/custom-min.css" type="text/css" rel="stylesheet" >
+		<!-- CSS  -->
+		<link href="min/plugin-min.css" type="text/css" rel="stylesheet">
+		<link href="min/custom-min.css" type="text/css" rel="stylesheet" >
 
- <!-- Timer -->
- <link rel="stylesheet" href="styleTimer.css"> 
-</head>
-<body id="top" class="scrollspy">
+		<!-- Timer -->
+		<link rel="stylesheet" href="styleTimer.css"> 
 
-  <!-- Pre Loader -->
-  <div id="loader-wrapper">
-    <div id="loader"></div>
+			<style>
+			@font-face {
+		    font-family: SamsungSharpSans;
+		    src: url(http://cdn.samsung.com/etc/designs/smg/global/fonts/SamsungSharpSansBd.woff);
+			}
+				h1 {font-family: "SamsungSharpSans";
+			    	font-weight: bold;
+			    	font-size: 66px;
+			    	color:#A020F0;
+			    	d 
+			    	line-height: 1.17;
+			    }
 
-    <div class="loader-section section-left"></div>
-    <div class="loader-section section-right"></div>
-  </div>
+			    h3 {font-family: "SamsungSharpSans";
+			    	font-weight: bold;
+			    	font-size: 30px;
+			    	color:#A020F0;
+			    	/*line-height: 1.17;*/
 
-  <!--Navigation-->
-  <?php 
-  require 'navigationBar.php'; 
-  ?>
-
-
-  <!--Event Details-->
-  <div class="section scrollspy" id="team">
-    <div class="container">
-      <h2 style="color:#A020F0">Interview 2017</h2>
-      <p>
-        "HELLO WORLD!" Codestrike is looking for enthusiastic students who wish to fluorish in the tech field and meet like minded people along the way!
-        <br> 
-        Have a chance to come across endless opportunities to learn/teach the most amazing tech stuff and brag about it in front of your friends! With the help of rock solid support of the best atharva seniors explore the tech world, you can learn things which are not in the curriculum but useful. The community conducts several meet ups in a month where members hangout and share about the stuff they learned. The recruitment process is fairly simple and there are no pre requisites to join codestrike. Register now and have a shot to be one of the skilled engineers you dreamed of.
-      </p>
-      <center>
-            <a href="Register2017.php" title="">Register Here</a>
-            <br>
-            <a href="test2017.php" title="">Login here</a>
-            </center>
-    </div>
-  </div>
-</div>
-</div>
-</div>
-</div>                          
-
-<!--Footer-->
-<?php 
-require 'footer.php';
-?>
-
-
-<!--  Scripts-->
-<script src="min/plugin-min.js"></script>
-<script src="min/custom-min.js"></script>
+			    }
 
 
 
-</body>
-</html>
+		    
+		    
+				
+			</style>
+
+	</head>
+	<body id="top" class="scrollspy">
+
+		<!-- Pre Loader -->
+		<div id="loader-wrapper">
+			<div id="loader"></div>
+
+			<div class="loader-section section-left"></div>
+			<div class="loader-section section-right"></div>
+		</div>
+
+		<!--Navigation-->
+		<?php 
+		require 'navigationBar.php'; 
+		?>
+
+
+		<!--Event Details-->
+		<div class="section scrollspy" id="team">
+			<div class="container">
+				<h2 style="color:#A020F0">Interview 2017</h2>
+				<p>
+					"HELLO WORLD!" Codestrike is looking for enthusiastic students who wish to fluorish in the tech field and meet like minded people along the way!
+					<br> 
+					Have a chance to come across endless opportunities to learn/teach the most amazing tech stuff and brag about it in front of your friends! With the help of rock solid support of the best atharva seniors explore the tech world, you can learn things which are not in the curriculum but useful. The community conducts several meet ups in a month where members hangout and share about the stuff they learned. The recruitment process is fairly simple and there are no pre requisites to join codestrike. Register now and have a shot to be one of the skilled engineers you dreamed of.
+				</p>
+				<br>
+
+				<center style="border-top: 1px solid black;">
+				
+				<h1>Selected Members</h1>
+				<h3>CodeStrike Interview Results</h3>
+					<table class="results_table">
+						<thead>
+						<tr>
+						<th>Name</th>
+						<th>Year</th>
+						<th>Branch</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Matthew Pazhur</td>
+							<td>SE</td>
+							<td>CMPN</td>
+						</tr>
+						<tr>
+							<td>Pawan Kumar</td>
+							<td>SE</td>
+							<td>CMPN</td>
+						</tr>
+						<tr>
+							<td>Arbinda</td>
+							<td>FE</td>
+							<td>CMPN</td>
+						</tr>
+						<tr>
+							<td>Rushikesh</td>
+							<td>FE</td>
+							<td>INFT</td>
+						</tr>
+						<tr>
+							<td>Abhijeet Shetty</td>
+							<td>SE</td>
+							<td>CMPN</td>
+						</tr>
+						<tr>
+							<td>Rishabh Shetty</td>
+							<td>FE</td>
+							<td>CMPN</td>
+						</tr>
+						<tr>
+							<td>Ishani Parkar</td>
+							<td>FE</td>
+							<td>EXTC</td>
+						</tr>
+						<tr>
+							<td>Suraj Pawar</td>
+							<td>-</td>
+							<td>CMPN</td>
+						</tr>
+						<tr>
+							<td>Saurabh Soparkar</td>
+							<td>FE</td>
+							<td>CMPN</td>
+						</tr>
+						<tr>
+							<td>Paras Singh</td>
+							<td>SE</td>
+							<td>CMPN</td>
+						</tr>
+						<tr>
+							<td>Ankit Vaiya</td>
+							<td>SE</td>
+							<td>INFT</td>
+						</tr>
+						<tr>
+							<td>Tejas Shetty</td>
+							<td>FE</td>
+							<td>ELEX</td>
+						</tr>
+					</tbody>
+					</table>
+				</center>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	</div>                          
+
+	<!--Footer-->
+	<?php 
+	require 'footer.php';
+	?>
+
+
+	<!--  Scripts-->
+	<script src="min/plugin-min.js"></script>
+	<script src="min/custom-min.js"></script>
+	<style type="text/css" media="screen">
+		.results_table{
+			width: 50%;
+		}
+	</style>
+
+
+
+	</body>
+	</html>
