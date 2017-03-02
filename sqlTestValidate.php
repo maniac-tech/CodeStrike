95bd4fe3c0596ea07659eb66132abdcb2b9b660f
Abhishek Jain <ajj_2396@yahoo.com>
2017-03-02 21:52:46 +0530

    Sql test

--- sqlTestValidate.php
+++ sqlTestValidate.php
@@ -1,24 +1,27 @@
 <?php 
 	require_once('sqlTestConnect.php');
 
-	$regStatus="";
+
 
 	if ($_SERVER["REQUEST_METHOD"]=="POST"){
 
+		echo "<br>If loop entered";
 		$stmt = $mysqli->prepare("INSERT INTO sqlTest VALUES (?, ?)");
 		$stmt->bind_param('ss', $name, $emailID);
 
+		echo "<br>Echo done";
 		$name=$_POST["fullName"];
 		$emailID=$_POST["emailID"];
-		
+
 		/* execute prepared statement */
 		$stmt->execute();
 
+		echo "<br>Post execution";
 		printf("%d Row inserted.\n", $stmt->affected_rows);
 
 		/* close statement and connection */
 		$stmt->close();
-		
+
 		/* close connection */
 		$mysqli->close();
 	}
