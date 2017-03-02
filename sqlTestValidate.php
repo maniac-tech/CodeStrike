9e2483f5d23299afc4fec47b098d29fd6da5af34
Abhishek Jain <ajj_2396@yahoo.com>
2017-03-02 22:11:58 +0530

    Update

--- sqlTestValidate.php
+++ sqlTestValidate.php
@@ -4,20 +4,34 @@
  	$regStatus="";
 
  	if ($_SERVER["REQUEST_METHOD"]=="POST"){
- 		$name=$_POST["fullName"];
- 		$emailID=$_POST["emailID"];
+ 		// $name=$_POST["fullName"];
+ 		// $emailID=$_POST["emailID"];
  		
 
- 		$query="INSERT INTO sqlTest (test1,test2) VALUES ('$name','$emailID')";
+ 		// $query="INSERT INTO sqlTest (test1,test2) VALUES ('$name','$emailID')";
 
- 		$result=$conn->query($query);
+ 		// $result=$conn->query($query);
 
- 		if($result){
- 			header("location:RegComplete.html");
- 		}
- 		else{
- 			header("location:RegFailed.html");
- 		}
+ 		// if($result){
+ 		// 	header("location:RegComplete.html");
+ 		// }
+ 		// else{
+ 		// 	header("location:RegFailed.html");
+ 		// }
+ 		// 
+ 		
+ 		$stmt = $mysqli->prepare("INSERT INTO sqlTest VALUES (?, ?)");
+$stmt->bind_param('ss', $name, $emailID);
+
+$name="stuffed";
+$emailID="Value";
+
+/* execute prepared statement */
+$stmt->execute();
+
+/* close statement and connection */
+$stmt->close();
+
  	}
  	else{
  		echo " <br>Error in validate!!";
