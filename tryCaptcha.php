3a6c3b586d5ed12317a6eef598a02b6f7b9cd3e2
maniac-tech <ajj_2396@yahoo.com>
2017-07-15 01:39:25 +0530

    Redirecting to validation page on success of captcha

--- tryCaptcha.php
+++ tryCaptcha.php
@@ -4,23 +4,16 @@ require_once('imacConnect.php');
 $regStatus="";
 
 if ($_SERVER["REQUEST_METHOD"]=="POST"){
+	// Captcha Validation
 	$captcha = $_POST['g-recaptcha-response'];
 	$captchaSecretKey=	getenv('GOOGLE_RECAPTCHA_SECRET');
 	$clientIp = $_SERVER['REMOTE_ADDR'];
 	$captchResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$captchaSecretKey."&response=".$captcha);
-	//$captchaResponseKeys = json_decode($response,true);
-	// echo $captchaResponseKeys;
-	echo "<script>console.log(".$responseKeys["success"]."</script>";
-	// if(intval($responseKeys["success"]) !== 1) {
-	// 	echo '<h2>SPAM</h2>';
-	// } else {
-	// 	echo '<h2>Genuine</h2>';
-	// }
 
 	if ($captchResponse.success==false){
 		echo "FAILED";
 	}
 	else{
-		echo "PASS";
+		header('Location:imacValidate.php');
 	}
 }
