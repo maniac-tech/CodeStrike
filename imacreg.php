7bf073d3053f509c0b8d0347feb0c5cb98671d90
maniac-tech <ajj_2396@yahoo.com>
2017-07-18 22:28:47 +0530

    included video into html

--- imacreg.php
+++ imacreg.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="js/jquery.min.js"></script>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-    <meta name="theme-color" content="#2196F3">
-    <title>iMac Training Sessions 2017</title>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.png" /> <!--Tab Icon-->
+  <script src="js/jquery.min.js"></script>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <meta name="theme-color" content="#2196F3">
+  <title>iMac Training Sessions 2017</title>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.png" /> <!--Tab Icon-->
 
-    <!-- CSS  -->
-    <link href="min/plugin-min.css" type="text/css" rel="stylesheet">
-    <link href="min/custom-min.css" type="text/css" rel="stylesheet" >
+  <!-- CSS  -->
+  <link href="min/plugin-min.css" type="text/css" rel="stylesheet">
+  <link href="min/custom-min.css" type="text/css" rel="stylesheet" >
 
-    <!-- Reg form CSS -->
-    <link rel="stylesheet" type="text/css" href="css/imacreg.css" media="all" />
-    <link rel="stylesheet" type="text/css" href="demo.css" media="all" />
+  <!-- Reg form CSS -->
+  <link rel="stylesheet" type="text/css" href="css/imacreg.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="demo.css" media="all" />
 
-    <!-- Google reCaptcha  -->
-    <script src='https://www.google.com/recaptcha/api.js'></script>  
+  <!-- Google reCaptcha  -->
+  <script src='https://www.google.com/recaptcha/api.js'></script>  
 </head>
 <body id="top" class="scrollspy">
 
@@ -27,24 +27,32 @@
 
     <div class="loader-section section-left"></div>
     <div class="loader-section section-right"></div>
-</div>
+  </div>
 
-<!--Navigation-->
-<?php 
-require 'navigationBar.php'; 
-?>
+  <!--Navigation-->
+  <?php 
+  require 'navigationBar.php'; 
+  ?>
 
 
-<!--Form Details-->
-<div class="section scrollspy" id="team">
+  <!--Form Details-->
+  <div class="section scrollspy" id="team">
     <br>
     <center>
       <h2 style="color:#A020F0; font-size: 35px">iMac Training Sessions 2017</h2>
     </center>
     <br>
     <div class="container">
+      <center>
+      <div class="video">
+      <video width="50%" controls="auto">
+        <source src="imacLab.mp4">
+        Your browser doesn't support the Video.
+      </video>
+      </div>
+      </center>
       <div  class="form">
-        <center><p>Register Here</p></center>
+      <center><p>Register Here</p></center>
         <hr>
         <form id="contactform" action="imacValidate.php" method="post"> 
           <p class="contact"><label for="name">First Name</label></p> 
@@ -67,31 +75,31 @@ require 'navigationBar.php';
             <option value="m">Male</option>
             <option value="f">Female</option>
             <option value="others">Other</option>
-        </select><br><br>
+          </select><br><br>
 
-        <p class="contact"><label for="phone">Mobile phone</label></p> 
-        <input id="phone" name="phone" placeholder="phone number" required="" type="text"> <br>
-        <!-- <span><div class="g-recaptcha" name="g-recaptcha-response" data-sitekey="6LeshBUUAAAAAOkHIrxVe2QetErIbIq2Bc5RQPxl"></div></span> -->
+          <p class="contact"><label for="phone">Mobile phone</label></p> 
+          <input id="phone" name="phone" placeholder="phone number" required="" type="text"> <br>
+          <!-- <span><div class="g-recaptcha" name="g-recaptcha-response" data-sitekey="6LeshBUUAAAAAOkHIrxVe2QetErIbIq2Bc5RQPxl"></div></span> -->
 
-        <!-- Google reCaptcha Code -->
-        <div class="g-recaptcha" data-sitekey="6LcPICkUAAAAAEgQZ7QvL0TNZkGx728-xsStXvoV"></div>
-        <br> 
-        <input class="buttom" name="submit" id="submit" tabindex="12" value="Register" type="submit" style="margin-left: 33%">   
-    </form> 
-</div>
+          <!-- Google reCaptcha Code -->
+          <div class="g-recaptcha" data-sitekey="6LcPICkUAAAAAEgQZ7QvL0TNZkGx728-xsStXvoV"></div>
+          <br> 
+          <input class="buttom" name="submit" id="submit" tabindex="12" value="Register" type="submit" style="margin-left: 33%">   
+        </form> 
+      </div>
 
-</div>
-</div>                         
+    </div>
+  </div>                         
 
-<!--Footer-->
-<?php 
-require 'footer.php';
-?>
+  <!--Footer-->
+  <?php 
+  require 'footer.php';
+  ?>
 
 
-<!--  Scripts-->
-<script src="min/plugin-min.js"></script>
-<script src="min/custom-min.js"></script>
+  <!--  Scripts-->
+  <script src="min/plugin-min.js"></script>
+  <script src="min/custom-min.js"></script>
 
 </body>
 </html>
