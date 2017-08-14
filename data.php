93577a1d1e7f9d7b6b1aa1180655b59feb7a7330
maniac-tech <ajj_2396@yahoo.com>
2017-08-14 23:00:54 +0530

    Created Data page for iMac

---
+++ data.php
@@ -0,0 +1,105 @@
+<?php 
+	require 'imacConnect.php';
+ ?>
+<html>
+<head>
+	<title>Sample Data</title>
+</head>
+<body>
+	<p>List of Students Registered</p>
+	<table border="1">
+		<tr>
+			<td>Name</td>
+			<td>Year</td>
+			<td>Branch</td>
+			<td>Email</td>
+			<td>Mobile</td>
+			<td>Status</td>
+		</tr>
+		<?php 
+			$sql = "SELECT * FROM $tableName ";
+			$result = mysqli_query($conn,$sql);
+
+			if(mysqli_num_rows($result)>0){
+				while ($row=mysqli_fetch_assoc($result)){
+					echo "<tr>";
+					echo "<td>".$row["Name"]."</td>";
+					echo "<td>".$row["Year"]."</td>";
+					echo "<td>".$row["Branch"]."</td>";
+					echo "<td>".$row["Email"]."</td>";
+					echo "<td>".$row["Mobile"]."</td>";	
+					echo "<td>".$row["Status"]."</td>";
+					echo "</tr>";			
+				}
+			}else{
+				echo "NO DATA";
+			}
+		?>
+		</table>
+	</table>
+
+<p>List of Students Completed</p>
+	<table border="1">
+		<tr>
+			<td>Name</td>
+			<td>Year</td>
+			<td>Branch</td>
+			<td>Email</td>
+			<td>Mobile</td>
+			<td>Status</td>
+		</tr>
+		<?php 
+			$sql = "SELECT * FROM $tableName WHERE Status='Complete'";
+			$result = mysqli_query($conn,$sql);
+
+			if(mysqli_num_rows($result)>0){
+				while ($row=mysqli_fetch_assoc($result)){
+					echo "<tr>";
+					echo "<td>".$row["Name"]."</td>";
+					echo "<td>".$row["Year"]."</td>";
+					echo "<td>".$row["Branch"]."</td>";
+					echo "<td>".$row["Email"]."</td>";
+					echo "<td>".$row["Mobile"]."</td>";	
+					echo "<td>".$row["Status"]."</td>";
+					echo "</tr>";			
+				}
+			}else{
+				echo "NO DATA";
+			}
+		?>
+		</table>
+	</table>	
+
+	<p>List of Students Pending</p>
+	<table border="1">
+		<tr>
+			<td>Name</td>
+			<td>Year</td>
+			<td>Branch</td>
+			<td>Email</td>
+			<td>Mobile</td>
+			<td>Status</td>
+		</tr>
+		<?php 
+			$sql = "SELECT * FROM $tableName WHERE Status='Pending'";
+			$result = mysqli_query($conn,$sql);
+
+			if(mysqli_num_rows($result)>0){
+				while ($row=mysqli_fetch_assoc($result)){
+					echo "<tr>";
+					echo "<td>".$row["Name"]."</td>";
+					echo "<td>".$row["Year"]."</td>";
+					echo "<td>".$row["Branch"]."</td>";
+					echo "<td>".$row["Email"]."</td>";
+					echo "<td>".$row["Mobile"]."</td>";	
+					echo "<td>".$row["Status"]."</td>";
+					echo "</tr>";			
+				}
+			}else{
+				echo "NO DATA";
+			}
+		?>
+		</table>
+	</table>
+</body>
+</html>
