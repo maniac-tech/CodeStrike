8c7fe1ec6e12ae9e38440af22492d0f84472fdd8
maniac-tech <ajj_2396@yahoo.com>
2017-08-14 23:00:21 +0530

    Created login page for dashboard

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="hq_style.css">
+</head>
+<body>
+<div class="login-page">
+  <div class="form">
+    <form class="login-form" method="post" action="hq_validate.php">
+      <input type="text" placeholder="username" name="loginID"/>
+      <input type="password" placeholder="password" name="loginPassword"/>
+      <input type="Submit" value="Login">
+      <p class="message">Not registered? <a href="membership2017.php">Create an account</a></p>
+    </form>
+  </div>
+</div>
+</body>
+</html>
