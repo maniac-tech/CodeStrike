f6dffe659bcc8a82509835c37145f91446b4952e
maniac-tech <ajj_2396@yahoo.com>
2017-08-19 01:15:49 +0530

    env var update

--- login_connect.php
+++ login_connect.php
@@ -2,8 +2,8 @@
 	$servername=getenv('DATABASE_SERVER_NAME_IMAC');
 	$databaseName=getenv('DATABASE_NAME_IMAC');
 	$tableName=getenv('DATABASE_TABLE_3');
-	$username=getenv('DATABASE_USERNAME');
-	$password=getenv('DATABASE_PASSWORD');
+	$username=getenv('DATABASE_USERNAME_IMAC');
+	$password=getenv('DATABASE_PASSWORD_IMAC');
 
 	// $servername='localhost';
 	// $username='root';
