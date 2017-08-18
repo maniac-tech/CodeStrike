76a05620c3e8f7fc633152feb9c618ea96ede36a
maniac-tech <ajj_2396@yahoo.com>
2017-08-19 00:36:50 +0530

    Added Buttons, made DB modifications

--- data.php
+++ data.php
@@ -1,90 +1,17 @@
 <?php
 require 'imacConnect.php';
 ?>
-	<p>List of Students Registered</p>
-	<table border="1">
-		<tr>
-			<td>Name</td>
-			<td>Year</td>
-			<td>Branch</td>
-			<td>Email</td>
-			<td>Mobile</td>
-			<td>Status</td>
-		</tr>
-		<?php
-		$sql = "SELECT * FROM $tableName ";
-		$result = mysqli_query($conn,$sql);
-		if(mysqli_num_rows($result)>0){
-			while ($row=mysqli_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$row["Name"]."</td>";
-				echo "<td>".$row["Year"]."</td>";
-				echo "<td>".$row["Branch"]."</td>";
-				echo "<td>".$row["Email"]."</td>";
-				echo "<td>".$row["Mobile"]."</td>";
-				echo "<td>".$row["Status"]."</td>";
-				echo "</tr>";
-			}
-		}else{
-			echo "NO DATA";
-		}
-		?>
-	</table>
-	<p>List of Students Completed</p>
-	<table border="1">
-		<tr>
-			<td>Name</td>
-			<td>Year</td>
-			<td>Branch</td>
-			<td>Email</td>
-			<td>Mobile</td>
-			<td>Status</td>
-		</tr>
-		<?php
-		$sql = "SELECT * FROM $tableName WHERE Status='Complete'";
-		$result = mysqli_query($conn,$sql);
-		if(mysqli_num_rows($result)>0){
-			while ($row=mysqli_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$row["Name"]."</td>";
-				echo "<td>".$row["Year"]."</td>";
-				echo "<td>".$row["Branch"]."</td>";
-				echo "<td>".$row["Email"]."</td>";
-				echo "<td>".$row["Mobile"]."</td>";
-				echo "<td>".$row["Status"]."</td>";
-				echo "</tr>";
-			}
-		}else{
-			echo "NO DATA";
-		}
-		?>
-	</table>
-	<p>List of Students Pending</p>
-	<table border="1">
-		<tr>
-			<td>Name</td>
-			<td>Year</td>
-			<td>Branch</td>
-			<td>Email</td>
-			<td>Mobile</td>
-			<td>Status</td>
-		</tr>
-		<?php
-		$sql = "SELECT * FROM $tableName WHERE Status='Pending'";
-		$result = mysqli_query($conn,$sql);
-		if(mysqli_num_rows($result)>0){
-			while ($row=mysqli_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$row["Name"]."</td>";
-				echo "<td>".$row["Year"]."</td>";
-				echo "<td>".$row["Branch"]."</td>";
-				echo "<td>".$row["Email"]."</td>";
-				echo "<td>".$row["Mobile"]."</td>";
-				echo "<td>".$row["Status"]."</td>";
-				echo "</tr>";
-			}
-		}else{
-			echo "NO DATA";
-		}
-		?>
-	</table>
+<div id="interface">
+	<button title="allStudents" onclick="loadContent(this.title)"> All Students</button>
+	<button title="pendingStudents" onclick="loadContent(this.title)">Pending Students</button>
+	<button title="completedStudents" onclick="loadContent(this.title)">Completed Students</button>
+	<button title="allotBatch" onclick="loadContent(this.title)">Allot Batch</button>
+	<button title="coordinators" onclick="loadContent(this.title)">Co-Ordinators</button>
+	<button title="batchDetails" onclick="loadContent(this.title)">Batch Details</button>
+	<button>Button 7</button>
+</div>
+<div id="content">
+	<div id="content_table">
+		<p>Some Introductory message</p>		
+	</div>
+</div>
