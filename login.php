8540bf1be22abce5be502e7dbdc5ff658615f2d7
maniac-tech <ajj_2396@yahoo.com>
2017-08-19 00:52:06 +0530

    created login panel with required connect and validate files

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php 
+  session_start();
+  if (isset($_SESSION['userId'])){
+    header('Location: admin.php');
+  }
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="hq_style.css">
+</head>
+<body>
+<div class="login-page">
+  <div class="form">
+    <form class="login-form" method="post" action="login_validate.php">
+      <input type="text" placeholder="username" name="loginID"/>
+      <input type="password" placeholder="password" name="loginPassword"/>
+      <input type="Submit" value="Login">
+      <!-- <p class="message">Not registered? <a href="membership2017.php">Create an account</a></p> -->
+    </form>
+  </div>
+</div>
+</body>
+</html>
---
+++ login_validate.php
@@ -0,0 +1,35 @@
+<?php
+	//include database config file 
+include('login_connect.php');
+//require_once('interview2017Connect.php');
+
+session_start();
+
+$id="0";
+$console='';
+$session_id='';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+	$id=$_POST["loginID"];
+	$password=$_POST["loginPassword"];
+
+	//create query
+	$query="SELECT * FROM users WHERE username='$id'";
+	$result=$conn->query($query);
+
+			//check the query in the database
+	if ($result){
+		if ($result->num_rows > 0){	
+			$row=mysqli_fetch_assoc($result);
+			if ($row['password']==$password){
+				// echo "LOGIN GRANTED";
+				$_SESSION['userId']=$row['username'];
+				header('Location:admin.php');
+			}else{
+				echo "<script>console.log('Login Denied')</script>";
+				header('Location:login.php');
+			}
+		}
+	}
+}
+?>
