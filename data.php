cbd3122e48d971fcef167713099683d99a1abbbf
maniac-tech <ajj_2396@yahoo.com>
2017-08-25 01:05:35 +0530

    Added Button 7 as Other Options

--- data.php
+++ data.php
@@ -8,7 +8,7 @@ require 'imacConnect.php';
 	<button title="allotBatch" onclick="loadContent(this.title)">Allot Batch</button>
 	<button title="coordinators" onclick="loadContent(this.title)">Co-Ordinators</button>
 	<button title="batchDetails" onclick="loadContent(this.title)">Batch Details</button>
-	<button>Button 7</button>
+	<button title="otherOptions" onclick="loadContent(this.title)">Other Options</button>
 </div>
 <div id="content">
 	<div id="content_table">
