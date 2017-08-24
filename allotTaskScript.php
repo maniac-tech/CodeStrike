f11af23e13e18ad8d87456c0220db1d95c84c85f
maniac-tech <ajj_2396@yahoo.com>
2017-08-24 10:49:24 +0530

    Added UPDATE Query on button submit

--- allotTaskScript.php
+++ allotTaskScript.php
@@ -3,12 +3,13 @@ require('imacConnect.php');
 if (!empty($_POST['checkbox'])){
 	$toInsert = $_POST['batchAllotedInput'];
 	foreach($_POST['checkbox'] as $check) {
-		$query="INSERT INTO $tableName (Batch) VALUES ('$toInsert') WHERE Mobile=$check";
-		$result=$conn->query($query);
+		$sql = "UPDATE $tableName SET Batch='$toInsert' WHERE Mobile=$check";
+		$result=mysqli_query($conn,$sql);
 		if ($result) {
 			echo "<p>QUERY COMPLETE</p>";
 		}else{
 			echo "<p>QUERY FAILED</p>";
+			echo mysqli_error($conn);
 		}
 		echo $check;
 	}
