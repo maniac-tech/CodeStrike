bb390da56358f047cf72f381c30e2f3fe089d6c9
maniac-tech <ajj_2396@yahoo.com>
2017-08-24 11:30:56 +0530

    Created outline for master php file

---
+++ master.php
@@ -0,0 +1,39 @@
+<?php 
+// Master PHP File
+// This file should conatin all the Functional working code for all the buttons. All at one Place
+
+/*
+Buttons are:
+1. All Students
+2. Pending Students
+3. Completed Students
+4. Allot Batch
+5. Co-Ordinators
+6. Batch Details
+7. Other Options
+*/
+
+/*
+Button Roles:
+1. All Students:
+	Display details of all the students irrelevant of the Status and Batch.
+
+2. Pending Students:
+	Display details of all the Students whose status is 'Pending'
+
+3. Completed Students:
+	Display details of all the Students whose status is 'Completed'
+
+4. Allot Batch:
+	Display list of Pending Students, and also, provide an interface to Allocate Batch number to these Students.
+
+5. Co-Ordinators:
+	List of Co-Orinators.
+
+6. Batch Details:
+	List of Students along with their Batch number and Status.
+
+7. Other Options:
+	Provide and Interface to Change the Status of Students from 'Pending' to 'Completed', and Vice-versa.
+*/
+ ?>
