08e07123e18be955482c5efdd528c54d12391c6c
maniac-tech <ajj_2396@yahoo.com>
2017-08-24 11:59:45 +0530

    Removed create an account option

--- login.php
+++ login.php
@@ -17,7 +17,6 @@
       <input type="text" placeholder="username" name="loginID"/>
       <input type="password" placeholder="password" name="loginPassword"/>
       <input type="Submit" value="Login">
-      <p class="message">Not registered? <a href="membership2017.php">Create an account</a></p>
     </form>
   </div>
 </div>
