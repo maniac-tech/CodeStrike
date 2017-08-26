4c2d6525102406a3377f0ea3477d14dcc1157d71
maniac-tech <ajj_2396@yahoo.com>
2017-08-26 22:02:30 +0530

    Added Instruction text

--- allotBatch.php
+++ allotBatch.php
@@ -4,7 +4,7 @@ require ('iMacConnect.php');
 
 <script>
 	function loadAjax(){
-var checkboxArray = new Array();
+		var checkboxArray = new Array();
 		var xhttp = new XMLHttpRequest();
 		
 		$("input:checked").each(function(){
@@ -24,18 +24,19 @@ var checkboxArray = new Array();
 </script>
 <div id="content_table">
 	<p id="studentsList"><b>List of Students Pending:</b></p>
+	<form action="" name="willAllotTaskForm" id="willAllotTaskForm">
+		<table >
 
-	<table >
-		<tr>
-			<td></td>
-			<td>Name</td>
-			<td>Year</td>
-			<td>Branch</td>
-		</tr>
-		<?php
-		$sql = "SELECT * FROM $tableName WHERE Status='Pending' AND Batch=0";
-		$result = mysqli_query($conn,$sql);
-		if(mysqli_num_rows($result)>0){
+			<?php
+			$sql = "SELECT * FROM $tableName WHERE Status='Pending' AND Batch=0";
+			$result = mysqli_query($conn,$sql);
+			if(mysqli_num_rows($result)>0){
+				echo "<tr>
+						<td></td>
+						<td>Name</td>
+						<td>Year</td>
+						<td>Branch</td>
+					</tr>";
 			while ($row=mysqli_fetch_assoc($result)){
 				echo "<tr>";
 				echo "<td><input type='checkbox' name='checkbox[]' id='checkbox' value='".$row['Mobile']."'></td>";
@@ -45,17 +46,21 @@ var checkboxArray = new Array();
 				echo "</tr>";
 			}
 		}else{
-			echo "NO DATA";
+			echo "ALL REGISTRATIONS HAVE BEEN ALLOTED A BATCH.";
 		}
 		?>
 	</table>
-
+</form>
 </div>
 <div id="operations">
 	<p id="studentsList">Instructions:</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus a corrupti libero ipsam unde. Impedit iure, deserunt odio! Eligendi beatae voluptatibus vel quo nostrum mollitia tenetur iusto, molestias quis! Non.</p>
+	<p><ol>
+		<li>Select Students to allot batch.</li>
+		<li>Enter their Batch no in the field Below.</li>
+		<li><b>NOTE:</b> No of Students selected at once, will be alloted the same Batch.</li>
+	</ol></p>
 	<p>
-		<input type="text" form="willAllotTaskForm" id="batchAllotedInput">
-		<button onclick="loadAjax()">Submit</button>
+		<input type="text" form="willAllotTaskForm" id="batchAllotedInput" required>
+		<button onclick="loadAjax()" form="willAllotTaskForm">Submit</button>
 	</p>
 </div>
