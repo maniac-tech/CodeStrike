385a7ce1a9adb8bf05fd59482ee4861871a25b89
maniac-tech <ajj_2396@yahoo.com>
2017-09-20 19:41:18 +0530

    Added regex to check for characters in username

--- login_validate.php
+++ login_validate.php
@@ -13,20 +13,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$id=$_POST["loginID"];
 	$password=$_POST["loginPassword"];
 
+	// Checking the received data for correct expressions:
+	if (!preg_match("/^[a-zA-Z]*$/",$id)){
+		echo "Enter only characters";
+	}else{
+
 	//create query
-	$query="SELECT * FROM users WHERE username='$id'";
-	$result=$conn->query($query);
+		$query="SELECT * FROM users WHERE username='$id'";
+		$result=$conn->query($query);
 
 			//check the query in the database
-	if ($result){
-		if ($result->num_rows > 0){	
-			$row=mysqli_fetch_assoc($result);
-			if ($row['password']==$password){
+		if ($result){
+			if ($result->num_rows > 0){	
+				$row=mysqli_fetch_assoc($result);
+				if ($row['password']==$password){
 				// echo "LOGIN GRANTED";
-				$_SESSION['userId']=$row['UserID'];
-				header('Location:adminTest.php');
-			}else{
-				echo "LOGIN DENIED";
+					$_SESSION['userId']=$row['UserID'];
+					header('Location:adminTest.php');
+				}else{
+					echo "LOGIN DENIED";
+				}
 			}
 		}
 	}
