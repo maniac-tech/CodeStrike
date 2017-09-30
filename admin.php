e9065abad2534ff7b1456d231a1e28ca1ef97e14
Abhishek Jain <ajj_2396@yahoo.com>
2017-10-01 01:37:32 +0530

    Dashboard (#50)
    * Created login page for dashboard
    
    * Created Data page for iMac
    
    * created basic login module
    
    * added css files for admin and data
    
    * added session variables
    
    * created logout script
    
    * added session variable
    
    * created new data files
    
    * certain css changes
    
    * removed db and added sample content
    
    * Added Buttons, made DB modifications
    
    * Added CSS styles for table and divs
    
    * Added submit button and inout element to enter batch
    
    * ADDED SCRIPT TO MANAGE CHECKBOX
    
    * Created other options tab
    
    * Added UPDATE Query on button submit
    
    * Added AND condition for SQL WHERE clause
    
    * Added Batch to display
    
    * Created outline for master php file
    
    * Removed create an account option
    
    * Added hyperlink to CodeStrike logo
    
    * Added the function to allocate batches to batches
    
    * Added AJAX and data Passinf via AJAX
    
    * added batch column
    
    * Added validation for batch selection
    
    * Added Button 7 as Other Options
    
    * Removed allotTaskScript.php
    
    * Minor change
    
    * Added Helvetica font
    
    * Added font face, font family,
    
    * Added properties to buttons
    
    * Added boc shadow
    
    * Added Intro screen text
    
    * Added Intruction text
    
    * Added Instruction text
    
    * Added Pending Button, relevant text on click, and instruction text
    
    * Added css to logout button,
    
    * Added font-family to content, and css to buttons
    
    * Added function to complete button
    
    * Added logout BUTTON tag and customised logo for CODESTRIKE
    
    * Removed bold from header
    
    * Added user icons and made some changes
    
    * Added form attribute to button tag
    
    * Added regex to check for characters in username
    
    * Removed the login validation code and replaced with crytpgraphy verification
    
    * Added db users
    
    * added the admin page code
    
    * removed vendor

--- admin.php
+++ admin.php
@@ -1,26 +1,50 @@
-<!DOCTYPE html>
+<?php 
+require 'imacConnect.php';
+session_start();
+if(!isset($_SESSION['userId'])){
+    header('Location:login.php');
+}
+?>
 <html>
-    <head>
-        <title>Admin - CodeStrike</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <style type="text/css">
-            body { text-align: center; padding: 10%; font: 20px Helvetica, sans-serif; color: #333; }
-            h1 { font-size: 50px; margin: 0; }
-            article { display: block; text-align: left; max-width: 650px; margin: 0 auto; }
-            a { color: #dc8100; text-decoration: none; }
-            a:hover { color: #333; text-decoration: none; }
-            @media only screen and (max-width : 480px) {
-                h1 { font-size: 40px; }
-            }
-        </style>
-    </head>
-    <body>
-        <article>
-            <h1>Page is under Development.</h1>
-            <!-- <p>Scheduled maintenance is currently in progress. Please check back soon.</p> -->
-            <!-- <p>We apologize for any inconvenience.</p> -->
-            <p id="signature">&mdash; <a href="mailto:[codestrikehq@gmail.com]">CodeStrike</a></p>
-        </article>
-    </body>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="css/adminTest.css">
+    <link rel="stylesheet" href="css/data.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+    <div id="navbar">
+        <p>Welcome, maniac-tech</p>
+        <form action="logout.php" method="POST">
+            <Button type="submit" name="Logout" value="Logout">Logout</Button>
+        </form>
+    </div>
+    <div id="sidebar">
+        <a href="http://www.codestrike.in">
+            <img src="img/dashboard_logo.png" alt="">
+        </a>
+        <ul>
+            <li title="overview" onclick="loadPage(this.title)">
+                <p>Overview</p>
+            </li>
+            <li title="data" onclick="loadPage(this.title)">
+                <p>iMac</p>
+            </li>
+        </ul>
+    </div>
+    <div id="body">
+        <?php 
+        require 'overview.php';
+        ?>
+    </div>
+</body>
+<script>
+    function loadPage(loadFile){
+        $('#body').load(loadFile+".php");
+    }
+    function loadContent(loadData){
+        $('#content').load(loadData+".php");
+    }
+</script>
+
 </html>
