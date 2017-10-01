3759e3d9f5c8b2a669de208ee254df21bfa40a60
maniac-tech <ajj_2396@yahoo.com>
2017-10-01 17:00:55 +0530

    Changed caps of Pending in SQL

--- pendingStudents.php
+++ pendingStudents.php
@@ -14,7 +14,7 @@ require ('imacConnect.php');
 			<td>Batch</td>
 		</tr>
 		<?php
-		$sql = "SELECT * FROM $tableName WHERE Status='Pending'";
+		$sql = "SELECT * FROM $tableName WHERE Status='PENDING'";
 		$result = mysqli_query($conn,$sql);
 		if(mysqli_num_rows($result)>0){
 			while ($row=mysqli_fetch_assoc($result)){
