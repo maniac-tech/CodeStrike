a4bfcecdc66239341e027037ed3619f4f3f73615
maniac-tech <ajj_2396@yahoo.com>
2017-10-01 21:36:29 +0530

    Added logo on the login page and created css file

--- login.php
+++ login.php
@@ -1,19 +1,19 @@
 <?php 
-  session_start();
-  if (isset($_SESSION['userId'])){
-    header('Location:admin.php');
-  }
- ?>
+session_start();
+if (isset($_SESSION['userId'])){
+  header('Location: adminTest.php');
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <title></title>
-  <link rel="stylesheet" type="text/css" href="hq_style.css">
+  <link rel="stylesheet" type="text/css" href="login.css">
 </head>
 <body>
   <div class="login-page">
     <div class="form">
-      <p><b>Log In</b></p>
+      <p><b><img src="img/dashboard_logo.png" alt="" style="width: 50%"></b></p>
       <form class="login-form" method="post" action="login_validate.php" id="loginForm">
         <div class="formgroup">
           <img src="img/icons/username.png" alt="">
@@ -23,7 +23,7 @@
           <img src="img/icons/password.png" alt="">
           <input type="password" placeholder="Password" name="loginPassword"/>
         </div>
-        <button type="Submit" value="Login" form="loginForm">Log In</button>
+        <button type="Submit" value="Login" form="loginForm">Login to Dashboard</button>
       </form>
       <!-- <a href="http://www.codestrike.in">CodeStrike</a> -->
     </div>
