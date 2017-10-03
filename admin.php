079b301540dc9fa1a9468054e5ab2dd192341b4c
maniac-tech <ajj_2396@yahoo.com>
2017-10-03 20:11:45 +0530

    Added relevant comments

--- admin.php
+++ admin.php
@@ -1,5 +1,11 @@
 <?php 
 require 'login_connect.php';
+/*
+    Checking for session variables, in order to validate the User's presence 
+    by the values stored using Session variables.
+    If the user's values are set, the dashboard will Load.
+    In case if the user is not logged in, he will be taken to login page.
+*/
 session_start();
 if(!isset($_SESSION['userId'])){
     header('Location:login.php');
@@ -13,12 +19,24 @@ if(!isset($_SESSION['userId'])){
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
+    <!--
+        The navbar is used to show the User Details, such as Username, Contact details, and profile options.
+        The navbar acts as the point of information for the user specific operation, such as profile, and logging out.
+
+        Although, the profile option is still under development. 
+    -->
     <div id="navbar">
         <p>Welcome, <?php echo $_SESSION['username']; ?></p>
         <form action="logout.php" method="POST">
             <Button type="submit" name="Logout" value="Logout">Logout</Button>
         </form>
     </div>
+    <!-- 
+        sidebar is used to move around the Dashboard.
+        It has the following tabs:
+            *Overview
+            *iMac
+     -->
     <div id="sidebar">
         <a href="http://www.codestrike.in" target="_blank">
             <img src="img/dashboard_logo.png" alt="">
@@ -46,5 +64,4 @@ if(!isset($_SESSION['userId'])){
         $('#content').load(loadData+".php");
     }
 </script>
-
 </html>
