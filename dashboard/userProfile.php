33eca5e5fdbb0813af55e3f665fb6dcc3a1a45bc
maniac-tech <ajj_2396@yahoo.com>
2017-10-07 01:03:04 +0530

    Created User profile

---
+++ dashboard/userProfile.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" href="css/userProfile.css">
+<div id="userProfile">
+  <div id="userHeader">
+    <h3 id="name">User Details</h3>
+    <A  id="edit">Edit Profile</A>
+  </div>
+  <div> 
+    <table>
+      <tbody>
+        <tr>
+          <td>Name:</td>
+          <td>Sheena Shrestha</td>
+        </tr>
+        <tr>
+          <td>Unique ID</td>
+          <td>XXXXXXXX</td>
+        </tr>
+        <tr>
+          <td>Department:</td>
+          <td>Programming</td>
+        </tr>
+
+        <tr>
+          <td>Date of Birth:</td>
+          <td>01/24/1988</td>
+        </tr>
+
+        <tr>
+         <tr>
+          <td>Gender:</td>
+          <td>Female</td>
+        </tr>
+        <tr>
+          <td>Email:</td>
+          <td><a href="mailto:info@support.com">info@support.com</a></td>
+        </tr>
+        <td>Phone Number:</td>
+        <td>555-4567-890(Mobile)</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
