6a2af7bc77c904f873f37fe110253d7475038034
Abhishek Jain <ajj_2396@yahoo.com>
2017-10-12 21:58:52 +0530

    Dashboard (#58)
    * Created login page for dashboard
    
    * Created Data page for iMac
    
    * created basic login module
    
    * added css files for admin and data
    
    * added session variables
    
    * created logout script
    
    * added session variable
    
    * created new data files
    
    * certain css changes
    
    * removed db and added sample content
    
    * Added Buttons, made DB modifications
    
    * Added CSS styles for table and divs
    
    * Added submit button and inout element to enter batch
    
    * ADDED SCRIPT TO MANAGE CHECKBOX
    
    * Created other options tab
    
    * Added UPDATE Query on button submit
    
    * Added AND condition for SQL WHERE clause
    
    * Added Batch to display
    
    * Created outline for master php file
    
    * Removed create an account option
    
    * Added hyperlink to CodeStrike logo
    
    * Added the function to allocate batches to batches
    
    * Added AJAX and data Passinf via AJAX
    
    * added batch column
    
    * Added validation for batch selection
    
    * Added Button 7 as Other Options
    
    * Removed allotTaskScript.php
    
    * Minor change
    
    * Added Helvetica font
    
    * Added font face, font family,
    
    * Added properties to buttons
    
    * Added boc shadow
    
    * Added Intro screen text
    
    * Added Intruction text
    
    * Added Instruction text
    
    * Added Pending Button, relevant text on click, and instruction text
    
    * Added css to logout button,
    
    * Added font-family to content, and css to buttons
    
    * Added function to complete button
    
    * Added logout BUTTON tag and customised logo for CODESTRIKE
    
    * Removed bold from header
    
    * Added user icons and made some changes
    
    * Added form attribute to button tag
    
    * Added regex to check for characters in username
    
    * Removed the login validation code and replaced with crytpgraphy verification
    
    * Added db users
    
    * added the admin page code
    
    * removed vendor
    
    * removed adminTest.php
    
    * Made changes acc to the DB
    
    * Update login_validate.php
    
    Testing the admin page
    
    * Added loginConnect
    
    * Corrected the session for userID
    
    * Added fail condition for wrong condition
    
    * Changed caps of 'Completed'
    
    * imacConnect
    
    * Added icons for the dashboard
    
    * Corrected the imacConnect script name
    
    * Changed caps of Pending in SQL
    
    * changed header
    
    * Added logo on the login page and created css file
    
    * open logo hyperlink in new tab
    
    * Added one more session variable
    
    * Printing the username on welcome screen
    
    * removed form attribute from submit button
    
    * Added DB credentials
    
    * Replaced dashboard users table with imac
    
    * renamed allstudents to all
    
    * renamed all to allStudents
    
    * Added guzzle, guzzle-php-http and sparkpost
    
    * Added guzzle, guzzle-php-http and sparkpost
    
    * Added relevant comments
    
    * removed previous interview files
    
    * Moved dashboard code files into dashboard folder
    
    * added DB connection values
    
    * Typing mistake in imacConnect file name cleared
    
    * removed allstudents as it was not being renamed
    
    * Renamed allstudents to allStudents
    
    * file name error in iMacConnect changed to imacConnect
    
    * moved master file into dashboard folder
    
    * Removed all the dashboard files after moving them into resp. folder
    
    * imacConnect name error
    
    * Added DB connection
    
    * Created table for coordinator list

--- dashboard/coordinators.php
+++ dashboard/coordinators.php
@@ -1,3 +1,36 @@
-<p>
-	Co-Ordinators List
-</p>
+<?php
+require ('../imacConnect.php'); 
+?>
+<div id="content_table">
+<p id="studentsList">Student Co-Ordinator List:</p>
+	<table >
+		<tr>
+			<th>Name</th>
+			<th>Year</th>
+			<th>Branch</th>
+			<th>Email</th>
+			<th>Mobile</th>
+			<th>Unique ID</th>
+			<th>RFID No</th>
+		</tr>
+		<?php
+		$sql = "SELECT * FROM $tableNameCo";
+		$result = mysqli_query($conn,$sql);
+		if(mysqli_num_rows($result)>0){
+			while ($row=mysqli_fetch_assoc($result)){
+				echo "<tr>";
+				echo "<td>".$row["Name"]."</td>";
+				echo "<td>".$row["Year"]."</td>";
+				echo "<td>".$row["Branch"]."</td>";
+				echo "<td>".$row["Email"]."</td>";
+				echo "<td>".$row["Mobile"]."</td>";
+				echo "<td>".$row["Unique ID"]."</td>";
+				echo "<td>".$row["RFID No"]."</td>";
+				echo "</tr>";
+			}
+		}else{
+			echo "NO DATA";
+		}
+		?>
+	</table>
+</div>
