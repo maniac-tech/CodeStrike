2fe5b4afd1fb0752c8651d400150380b7a6384f3
Abhishek Jain <ajj_2396@yahoo.com>
2017-10-15 00:56:22 +0530

    Dashboard (#64)
    * Created login page for dashboard
    
    * Created Data page for iMac
    
    * created basic login module
    
    * added css files for admin and data
    
    * added session variables
    
    * created logout script
    
    * added session variable
    
    * created new data files
    
    * certain css changes
    
    * removed db and added sample content
    
    * Added Buttons, made DB modifications
    
    * Added CSS styles for table and divs
    
    * Added submit button and inout element to enter batch
    
    * ADDED SCRIPT TO MANAGE CHECKBOX
    
    * Created other options tab
    
    * Added UPDATE Query on button submit
    
    * Added AND condition for SQL WHERE clause
    
    * Added Batch to display
    
    * Created outline for master php file
    
    * Removed create an account option
    
    * Added hyperlink to CodeStrike logo
    
    * Added the function to allocate batches to batches
    
    * Added AJAX and data Passinf via AJAX
    
    * added batch column
    
    * Added validation for batch selection
    
    * Added Button 7 as Other Options
    
    * Removed allotTaskScript.php
    
    * Minor change
    
    * Added Helvetica font
    
    * Added font face, font family,
    
    * Added properties to buttons
    
    * Added boc shadow
    
    * Added Intro screen text
    
    * Added Intruction text
    
    * Added Instruction text
    
    * Added Pending Button, relevant text on click, and instruction text
    
    * Added css to logout button,
    
    * Added font-family to content, and css to buttons
    
    * Added function to complete button
    
    * Added logout BUTTON tag and customised logo for CODESTRIKE
    
    * Removed bold from header
    
    * Added user icons and made some changes
    
    * Added form attribute to button tag
    
    * Added regex to check for characters in username
    
    * Removed the login validation code and replaced with crytpgraphy verification
    
    * Added db users
    
    * added the admin page code
    
    * removed vendor
    
    * removed adminTest.php
    
    * Made changes acc to the DB
    
    * Update login_validate.php
    
    Testing the admin page
    
    * Added loginConnect
    
    * Corrected the session for userID
    
    * Added fail condition for wrong condition
    
    * Changed caps of 'Completed'
    
    * imacConnect
    
    * Added icons for the dashboard
    
    * Corrected the imacConnect script name
    
    * Changed caps of Pending in SQL
    
    * changed header
    
    * Added logo on the login page and created css file
    
    * open logo hyperlink in new tab
    
    * Added one more session variable
    
    * Printing the username on welcome screen
    
    * removed form attribute from submit button
    
    * Added DB credentials
    
    * Replaced dashboard users table with imac
    
    * renamed allstudents to all
    
    * renamed all to allStudents
    
    * Added guzzle, guzzle-php-http and sparkpost
    
    * Added guzzle, guzzle-php-http and sparkpost
    
    * Added relevant comments
    
    * removed previous interview files
    
    * Moved dashboard code files into dashboard folder
    
    * added DB connection values
    
    * Typing mistake in imacConnect file name cleared
    
    * removed allstudents as it was not being renamed
    
    * Renamed allstudents to allStudents
    
    * file name error in iMacConnect changed to imacConnect
    
    * moved master file into dashboard folder
    
    * Removed all the dashboard files after moving them into resp. folder
    
    * imacConnect name error
    
    * Added DB connection
    
    * Created table for coordinator list
    
    * Added nominated students section
    
    * Removed Some colums not required for nominated students
    
    * Pie Chart using Google Charts API (#60)
    
    * Charts (#63)
    
    * Pie Chart using Google Charts API
    
    * Changed the branch names to their short forms
    
    * Made the piechart look 3D
    
    * Changed width of piechart div and added barchart for year data
    
    * Added div for bar graph
    
    * Added width and height to piechart div
    
    * Created CSS file for overview.php

--- dashboard/overview.php
+++ dashboard/overview.php
@@ -1,13 +1,15 @@
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<div id="piechart" style="width: 900px; height: 500px;"></div>
+<div id="piechart" ></div>
+<div id="chart_div"></div>
 <?php 
 	$sql="";
 	$result="";
-	$sql = "SELECT * FROM $tableName WHERE Branch='Cmpn' ";
+	$sql = "SELECT * FROM $tableName WHERE Branch='CMPN' ";
 	$result = mysqli_query($conn,$sql);
 	$countCMPN = mysqli_num_rows($result);
 
-	$sql = "SELECT * FROM $tableName WHERE Branch='IT' ";
+	$sql = "SELECT * FROM $tableName WHERE Branch='INFT' ";
+
 	$result = mysqli_query($conn,$sql);
 	$countIT = mysqli_num_rows($result);
 
@@ -15,18 +17,30 @@
 	$result = mysqli_query($conn,$sql);
 	$countEXTC = mysqli_num_rows($result);
 
-	$sql = "SELECT * FROM $tableName WHERE Branch='Electronics' ";
+	$sql = "SELECT * FROM $tableName WHERE Branch='ELEX' ";
 	$result = mysqli_query($conn,$sql);
 	$countELEC = mysqli_num_rows($result);	
 
-	$sql = "SELECT * FROM $tableName WHERE Branch='Electrical' ";
+	$sql = "SELECT * FROM $tableName WHERE Branch='ELEC' ";
 	$result = mysqli_query($conn,$sql);
 	$countELEX = mysqli_num_rows($result);	
 
- ?>
+	$sql = "SELECT * FROM $tableName WHERE Year='BE' ";
+	$result = mysqli_query($conn,$sql);
+	$countBE = mysqli_num_rows($result);
+
+	$sql = "SELECT * FROM $tableName WHERE Year='TE' ";
+	$result = mysqli_query($conn,$sql);
+	$countTE = mysqli_num_rows($result);
+
+	$sql = "SELECT * FROM $tableName WHERE Year='SE' ";
+	$result = mysqli_query($conn,$sql);
+	$countSE = mysqli_num_rows($result);
+?>
 <script>
 	google.charts.load('current', {'packages':['corechart']});
 	google.charts.setOnLoadCallback(drawChart);
+	google.charts.setOnLoadCallback(drawBasic);
 
 	function drawChart() {
 
@@ -40,10 +54,37 @@
 			]);
 
 		var options = {
-			title: 'Department wise Registrations'
+			title: 'Department wise Registrations',
+			is3D: true,
 		};
 
 		var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+		chart.draw(data, options);
+	}
+  
+	function drawBasic() {
+
+		var data = google.visualization.arrayToDataTable([
+			['Year', 'Registrations',],
+			['SE', <?php echo $countSE; ?>],
+			['TE', <?php echo $countTE; ?>],
+			['BE', <?php echo $countBE; ?>],
+			]);
+
+		var options = {
+			title: 'Year wise Registrations',
+			chartArea: {width: '50%'},
+			hAxis: {
+				title: 'Total Registrations',
+				minValue: 0
+			},
+			vAxis: {
+				title: 'Year'
+			}
+		};
+
+		var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
 
 		chart.draw(data, options);
 	}
