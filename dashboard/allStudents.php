03caac397207101cb67418c18aa50e2e60ae276d
Harshit Shetty <swaharsh@gmail.com>
2017-11-04 13:41:44 +0530

    search bar

--- dashboard/allStudents.php
+++ dashboard/allStudents.php
@@ -6,6 +6,10 @@ if(!isset($_SESSION['userId'])){
 }
 ?>
 <div id="content_table">
+	<form action="" method="post">
+<input type="text" name="search">
+<input type="submit" name="submit" value="Search">
+</form>
 <p id="studentsList">List of Students Registered:</p>
 	<table >
 		<tr>
@@ -17,6 +21,44 @@ if(!isset($_SESSION['userId'])){
 			<th>Status</th>
 			<th>Batch</th>
 		</tr>
+		<tr>
+			<td>Name</td>
+			<td>Year</td>
+			<td>Branch</td>
+			<td>Email</td>
+			<td>Mobile</td>
+			<td>Status</td>
+			<td>Batch</td>
+		</tr>
+		<tr>
+			<td>Name</td>
+			<td>Year</td>
+			<td>Branch</td>
+			<td>Email</td>
+			<td>Mobile</td>
+			<td>Status</td>
+			<td>Batch</td>
+		</tr>
+		<?php
+		$search_value=$_POST["search"];
+		$sql="SELECT * FROM $tableName where Name like '%$search_value%'";
+		$result = mysqli_query($conn,$sql);
+		if(mysqli_num_rows($result)>0){
+			while ($row=mysqli_fetch_assoc($result)){
+				echo "<tr>";
+				echo "<td>".$row["Name"]."</td>";
+				echo "<td>".$row["Year"]."</td>";
+				echo "<td>".$row["Branch"]."</td>";
+				echo "<td>".$row["Email"]."</td>";
+				echo "<td>".$row["Mobile"]."</td>";
+				echo "<td>".$row["Status"]."</td>";
+				echo "<td>".$row["Batch"]."</td>";
+				echo "</tr>";
+			}
+		}else{
+			echo "NO DATA";
+		}
+		?>
 		<?php
 		$sql = "SELECT * FROM $tableName ";
 		$result = mysqli_query($conn,$sql);
