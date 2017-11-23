9ce50e95682df69e8d7d840b1168eac2f5e0e43a
maniac-tech <ajj_2396@yahoo.com>
2017-11-23 23:58:09 +0530

    QUERY updated

--- dashboard/login_validate.php
+++ dashboard/login_validate.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 		// $query="SELECT * FROM $tableName WHERE username='$id'";
 		// $result = mysqli_query($conn,$query);
 
-		$result = pg_query_params($dbconn, "SELECT * FROM $tableName WHERE username='$1'", array($id));
+		$result = pg_query_params($dbconn, "SELECT * FROM $tableName WHERE userID='$1'", array($id));
 		echo "result done";
 		//check the query in the database
 		/*
