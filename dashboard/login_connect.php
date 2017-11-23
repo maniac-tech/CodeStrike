b2999cd393a9c5001e493f1d820df44fbbfd664d
maniac-tech <ajj_2396@yahoo.com>
2017-11-23 23:25:42 +0530

    Syntax error of missing ';' found

--- dashboard/login_connect.php
+++ dashboard/login_connect.php
@@ -7,7 +7,7 @@ $username=getenv('PostGRE_DB_User');
 $password=getenv('PostGRE_DB_Password');
 // $tableName=getenv('DATABASE_TABLE_NAME_DASHBOARDUSERS');
 
-$dbconn = pg_connect("host=$servername dbname=$databaseName user=$username password=$password")
+$dbconn = pg_connect("host=$servername dbname=$databaseName user=$username password=$password");
 if (!$dbconn){
 	echo ('Could not connect: ' . pg_last_error().'<br>');
 	echo pg_result_error($dbconn);
