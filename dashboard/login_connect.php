f5198d409095a733d18ca9348545802d27395a67
maniac-tech <ajj_2396@yahoo.com>
2017-11-23 23:19:23 +0530

    Issue in login_connect.php, trying to resolve it

--- dashboard/login_connect.php
+++ dashboard/login_connect.php
@@ -1,6 +1,6 @@
 <?php
 // Connecting, selecting database
-
+echo "login_connect.php:<br>";
 $servername=getenv('PostGRE_DB_Host');
 $databaseName=getenv('PostGRE_DB');
 $username=getenv('PostGRE_DB_User');
