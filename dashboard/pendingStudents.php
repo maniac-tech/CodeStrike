812b316394e351f3a34f64eb53d96fe9f4d97922
maniac-tech <ajj_2396@yahoo.com>
2017-11-26 00:04:24 +0530

    Replaced query with query_aparams

--- dashboard/pendingStudents.php
+++ dashboard/pendingStudents.php
@@ -19,8 +19,8 @@ if(!isset($_SESSION['userId'])){
 		</tr>
 		<?php
 		//----- PostGRE SQL Commands -----
-		$query = "SELECT * FROM $tablename_IMac WHERE Status='PENDING'";
-		$result = pg_query($dbconn, $query);
+		$query = "SELECT * FROM $tablename_IMac WHERE Status='$1'";
+		$result = pg_query_params($dbconn, $query,array('PENDING'));
 		if (pg_result_status($result)==2) {
 			while($row = pg_fetch_assoc($result)){
 				echo "<tr>";
