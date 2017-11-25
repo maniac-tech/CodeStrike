aa80300461c94d60422d6ec612971bfc6fdbb004
maniac-tech <ajj_2396@yahoo.com>
2017-11-26 00:01:39 +0530

    Added PostGRE code for Pending Students Section

--- dashboard/pendingStudents.php
+++ dashboard/pendingStudents.php
@@ -14,10 +14,33 @@ if(!isset($_SESSION['userId'])){
 			<td>Branch</td>
 			<td>Email</td>
 			<td>Mobile</td>
-			<td>Status</td>
-			<td>Batch</td>
+			<!-- TO-DO: Print the Batch no alloted from the respective table -->
+			<!-- <th>Batch</th> -->
 		</tr>
 		<?php
+		//----- PostGRE SQL Commands -----
+		$query = "SELECT * FROM $tablename_IMac WHERE Status='PENDING'";
+		$result = pg_query($dbconn, $query);
+		if (pg_result_status($result)==2) {
+			while($row = pg_fetch_assoc($result)){
+				echo "<tr>";
+				echo "<td>".$row["Name"]."</td>";
+				echo "<td>".$row["Year"]."</td>";
+				echo "<td>".$row["Branch"]."</td>";
+				echo "<td>".$row["Email"]."</td>";
+				echo "<td>".$row["Mobile"]."</td>";
+				// echo "<td>".$row["Status"]."</td>";
+				// echo "<td>".$row["Batch"]."</td>";
+				echo "</tr>";
+			}
+		}
+		else{
+			echo "Query Failed.";
+		}
+		// -X-X-X- End of PostGRE SQL Commands -X-X-X-
+
+		//----- SQL Commands -----
+		/*
 		$sql = "SELECT * FROM $tableName WHERE Status='Pending'";
 		$result = mysqli_query($conn,$sql);
 		if(mysqli_num_rows($result)>0){
@@ -35,6 +58,8 @@ if(!isset($_SESSION['userId'])){
 		}else{
 			echo "NO DATA";
 		}
+		*/
+		// -X-X-X- End of SQL Commands -X-X-X-
 		?>
 	</table>
 </div>
