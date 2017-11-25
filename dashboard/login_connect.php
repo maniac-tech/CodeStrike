e28adb75e8d8648cdd755cf12b5315d0d45a0e0c
maniac-tech <ajj_2396@yahoo.com>
2017-11-25 20:51:22 +0530

    Added IMAC table details to connection file

--- dashboard/login_connect.php
+++ dashboard/login_connect.php
@@ -5,6 +5,7 @@ $databaseName=getenv('PostGRE_DB');
 $username=getenv('PostGRE_DB_User');
 $password=getenv('PostGRE_DB_Password');
 $tableName=getenv('PostGRE_DB_Users');
+$tablename_IMac=getenv('PostGRE_DB_IMac');
 
 $dbconn = pg_connect("host=$servername dbname=$databaseName user=$username password=$password");
 if (!$dbconn){
