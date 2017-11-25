f70852cec6e9f1948908ae6d2590246db5b41b5f
maniac-tech <ajj_2396@yahoo.com>
2017-11-25 20:41:10 +0530

    Shifted to PostGRE SQL

--- dashboard/allStudents.php
+++ dashboard/allStudents.php
@@ -18,6 +18,30 @@ if(!isset($_SESSION['userId'])){
 			<th>Batch</th>
 		</tr>
 		<?php
+		//----- PostGRE SQL Commands -----
+		$query = "SELECT * FROM $tableName ";
+		$result = pg_query_params($dbconn, $query, array());
+
+		if (pg_result_status($result)==2) {
+			while($row = pg_fetch_assoc($result)){
+				echo "<tr>";
+				echo "<td>".$row["Name"]."</td>";
+				echo "<td>".$row["Year"]."</td>";
+				echo "<td>".$row["Branch"]."</td>";
+				echo "<td>".$row["Email"]."</td>";
+				echo "<td>".$row["Mobile"]."</td>";
+				echo "<td>".$row["Status"]."</td>";
+				echo "<td>".$row["Batch"]."</td>";
+				echo "</tr>";
+			}
+		}
+		else{
+			echo "Query Failed.";
+		}
+		// -X-X-X- End of PostGRE SQL Commands -X-X-X-
+
+		//----- SQL Commands -----
+		/*
 		$sql = "SELECT * FROM $tableName ";
 		$result = mysqli_query($conn,$sql);
 		if(mysqli_num_rows($result)>0){
@@ -35,6 +59,8 @@ if(!isset($_SESSION['userId'])){
 		}else{
 			echo "NO DATA";
 		}
+		*/
+		// -X-X-X- End of SQL Commands -X-X-X-
 		?>
 	</table>
 </div>
