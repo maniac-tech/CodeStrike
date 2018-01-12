40db2d4c869583898204e3a2ea9e9b88fa19d6bb
maniac-tech <ajj_2396@yahoo.com>
2018-01-12 23:54:55 +0530

    Redirected the page on success.

--- imac/imacValidate.php
+++ imac/imacValidate.php
@@ -20,7 +20,7 @@ function insertData($func_var_name,$func_var_year,$func_var_branch,$func_var_ema
 	global $conn;
 
 	//----- PostGRE SQL Commands -----
-	$query="INSERT INTO $tableName_interviews (\"Name\",\"Year\",\"Branch\",\"Email\",\"Mobile\") VALUES ('$func_var_name','$func_var_year','$func_var_branch','$func_var_emailID','$func_var_mobileNo')";
+	$query="INSERT INTO $tableName_interviews (\"Name\",\"Year\",\"Branch\",\"Email\",\"Mobile\") VALUES ('$func_var_name','$func_var_year','2','$func_var_emailID','$func_var_mobileNo')";
 	$result=pg_query($dbconn,$query);
 	// -X-X-X- End of PostGRE SQL Commands -X-X-X-
 
