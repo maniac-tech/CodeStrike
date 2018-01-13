a5a215f9f5c14b5c092f60bfc1f5f0708d9d884c
maniac-tech <ajj_2396@yahoo.com>
2018-01-14 00:12:41 +0530

    Debugging...

---
+++ imac/temp.php
@@ -0,0 +1,30 @@
+<?php 
+
+	$servername=getenv('PostGRE_DB_Host');
+	$databaseName=getenv('PostGRE_DB');
+	$username=getenv('PostGRE_DB_User');
+	$password=getenv('PostGRE_DB_Password');
+	$tableName_interviews = getenv('PostGRE_DB_IMac_intr');
+
+	// $servername="localhost";
+	// $databaseName="imac";
+	// $tableName;
+	// $username="root";
+	// $password="";
+
+	//create connection
+	$dbconn = pg_connect("host=$servername dbname=$databaseName user=$username password=$password");
+	if (!$dbconn){
+		echo ('Could not connect: ' . pg_last_error().'<br>');
+		echo pg_result_error($dbconn);
+	}else{
+		$query = "SELECT * FROM $tableName_interviews";
+		$result = pg_query($dbconn,$query);
+		if ($result){
+			echo "Yayy!";
+		}
+		else{
+			echo "Oops!!";
+		}
+	}
+	?>
