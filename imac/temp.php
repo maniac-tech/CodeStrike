c5a5dc93fdd8df792d44fd8957b3348c0f1b8284
maniac-tech <ajj_2396@yahoo.com>
2018-01-14 00:16:50 +0530

    Debugging...

--- imac/temp.php
+++ imac/temp.php
@@ -18,7 +18,7 @@
 		echo ('Could not connect: ' . pg_last_error().'<br>');
 		echo pg_result_error($dbconn);
 	}else{
-		$query = "SELECT * FROM $tableName_interviews";
+		$query="INSERT INTO '$tableName_'.\interviews (Name,Year,Branch,Email,Mobile) VALUES ('Abhishek Jain','BE','CMPN','ajj.2396@gmail.com','9833377552')";
 		$result = pg_query($dbconn,$query);
 		if ($result){
 			echo "Yayy!";
