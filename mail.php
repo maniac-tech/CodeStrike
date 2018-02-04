3ca24403b6f858cfa52c24bffd5e85a9ca5682fa
maniac-tech <ajj_2396@yahoo.com>
2018-02-05 00:07:49 +0530

    Added style to template

--- mail.php
+++ mail.php
@@ -15,7 +15,7 @@ $promise = $sparky->transmissions->post([
         ],
         'subject' => 'First Mailing From PHP',
         'html' => '<html>
-        <body style="background-color: grey; width:100%;">
+        <body style="background-color: grey; width:100%; font-family: sans-serif;">
         <table style="background-color: white;margin-left: 5%; margin-right: 5%; margin-top: 5%;margin-bottom: 5%;width: 90%;">
         <tr >
         <td style="width: 100%; border-bottom: solid; border-color: grey; border-width: 1%;">
@@ -26,14 +26,14 @@ $promise = $sparky->transmissions->post([
         </tr>
         <tr >
         <td>
-        <p>
+        <p style="margin-top: 2%; margin-left:2%;margin-right: 2%;">
         Hi User,
         </p>
-        <p>
+        <p style="margin-left:2%; margin-right: 2%;margin-bottom: 2%;">
         Your Registration has been confirmed.
-        You will be receiving the batch details and other event details soon via mail.
-        Make sure you check your Inbox and <b>Spam</b> folder regularly.
-        <br>
+        You will be receiving the batch and traingin details soon via mail.
+        Please make sure you check your <b>Inbox</b> and <b>Spam</b> folder regularly.
+        <br><br>
         Thank You,<br>
         Team CodeStrike.
         </p>
