83492989b6cf3ef7613a56d8f3737d58f70ceb33
maniac-tech <ajj_2396@yahoo.com>
2018-02-05 01:37:03 +0530

    Removed the mail segment

--- imac/imacValidate.php
+++ imac/imacValidate.php
@@ -1,10 +1,6 @@
 <?php 
 // Importing all the required files:
 require_once('imacConnect.php');
-require 'vendor/autoload.php';
-use SparkPost\SparkPost;
-use GuzzleHttp\Client;
-use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
 
 // Declaring Global variables: is for False nd 1 is for True
 $status_captcha;
@@ -54,7 +50,8 @@ function insertData($func_var_name,$func_var_year,$func_var_branch,$func_var_ema
 			// echo "Successful submission of form";
 		}
 		else{
-			echo "pg_querya output:".pg_query($dbconn,$query);
+			// echo "pg_querya output:".pg_query($dbconn,$query);
+			header('Location:http://www.google.com');
 		}
 	}
 }
@@ -154,91 +151,30 @@ function regularExpression(){
  				}
  			}
  		}
- }
+}
 
 
- function sendMail (){
- 	$httpClient = new GuzzleAdapter(new Client());
- 	$sparky = new SparkPost($httpClient, ["key" => getEnv("SPARKPOST_API_KEY_IMAC_DELIEVERY")]);
- 	$promise = $sparky->transmissions->post([
- 		'content' => [
- 			'from' => [
- 				'name' => 'SparkPost Team',
- 				'email' => 'imac@codestrike.in',
- 			],
- 			'subject' => 'First Mailing From PHP',
- 			'html' => '<html>
- 			<body style="background-color: grey; width:100%; font-family: sans-serif;">
- 			<table style="background-color: white;margin-left: 5%; margin-right: 5%; margin-top: 5%;margin-bottom: 5%;width: 90%;">
- 			<tr >
- 			<td style="width: 100%; border-bottom: solid; border-color: grey; border-width: 1%;">
- 			<center>
- 			<img src="http://www.codestrike.in/img/logo.png" alt="" style="width: 25%; align-content: center;">
- 			</center>
- 			</td>
- 			</tr>
- 			<tr >
- 			<td>
- 			<p style="margin-top: 2%; margin-left:2%;margin-right: 2%;">
- 			Hi User,
- 			</p>
- 			<p style="margin-left:2%; margin-right: 2%;margin-bottom: 2%;">
- 			Your Registration has been confirmed.
- 			You will be receiving the batch and traingin details soon via mail.
- 			Please make sure you check your <b>Inbox</b> and <b>Spam</b> folder regularly.
- 			<br><br>
- 			Thank You,<br>
- 			Team CodeStrike.
- 			</p>
- 			</td>
- 			</tr>
- 			</table>
- 			</body>
- 			</html>',
-        // 'text' => 'Congratulations, {{name}}! You just sent your very first mailing!',
- 		],
- 		'substitution_data' => ['name' => 'iMac'],
- 		'recipients' => [
- 			[
- 				'address' => [
- 					'name' => 'Abhishek Jain',
- 					'email' => 'ajj.2396@gmail.com',
- 				],
- 			],
- 		],
- 	]);
- 	try {
- 		$response = $promise->wait();
- 		echo "try:";
- 		echo $response->getStatusCode()."\n";
- 		print_r($response->getBody())."\n";
- 	} catch (\Exception $e) {
- 		echo $e->getCode()."\n";
- 		echo $e->getMessage()."\n";
- 	}
- }
+function error($func_var_mobileNo){
+	global $form_fname, $form_lname, $form_name, $form_emailId, $form_mobileNo, $form_year, $form_branch;
+	global $tableName_imacTraining, $dbconn;
+	$sql = "SELECT Name FROM $tableName WHERE Mobile='$func_var_mobileNo'";
+	$result = mysqli_query($conn, $sql);
 
- function error($func_var_mobileNo){
- 	global $form_fname, $form_lname, $form_name, $form_emailId, $form_mobileNo, $form_year, $form_branch;
- 	global $tableName_imacTraining, $dbconn;
- 	$sql = "SELECT Name FROM $tableName WHERE Mobile='$func_var_mobileNo'";
- 	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows ($result) > 0){
+		echo "You have been Registered already.";
+	}
+	else{
+		echo "Registration Failed. Try Again. If the problem still occurs, contact the iMac Incharge.<br>";
+		echo $conn->error;
+	}
+}
 
- 	if (mysqli_num_rows ($result) > 0){
- 		echo "You have been Registered already.";
- 	}
- 	else{
- 		echo "Registration Failed. Try Again. If the problem still occurs, contact the iMac Incharge.<br>";
- 		echo $conn->error;
- 	}
- }
+function main(){
+	if($_SERVER["REQUEST_METHOD"]=="POST"){
+		$status_captcha = captchaValidation();
+	}
+}
 
- function main(){
- 	if($_SERVER["REQUEST_METHOD"]=="POST"){
- 		$status_captcha = captchaValidation();
- 	}
- }
+main();
 
- main();
-
- ?>
+?>
