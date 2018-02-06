ff0e7bf4296615f4bb02e6d62f6ed8a2b5285540
maniac-tech <ajj_2396@yahoo.com>
2018-02-06 19:54:18 +0530

    Added a static file name for file download on all students page

--- dashboard/allStudents.php
+++ dashboard/allStudents.php
@@ -71,7 +71,7 @@ if(!isset($_SESSION['userId'])){
 		headers: true,                              // (Boolean), display table headers (th or td elements) in the <thead>, (default: true)
 	    footers: true,                              // (Boolean), display table footers (th or td elements) in the <tfoot>, (default: false)
 	    formats: ['xls', 'csv', 'txt'],             // (String[]), filetype(s) for the export, (default: ['xls', 'csv', 'txt'])
-    	filename: 'id',                             // (id, String), filename for the downloaded file, (default: 'id')
+    	filename: 'All Students',                             // (id, String), filename for the downloaded file, (default: 'id')
    		bootstrap: true,                           // (Boolean), style buttons using bootstrap, (default: true)
     	exportButtons: true,                        // (Boolean), automatically generate the built-in export buttons for each of the specified formats (default: true)
     	position: 'bottom',                         // (top, bottom), position of the caption element relative to table, (default: 'bottom')
