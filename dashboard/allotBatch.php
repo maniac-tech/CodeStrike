3afb213c2415039077b95345fc9a71bfc989d4c5
maniac-tech <ajj_2396@yahoo.com>
2018-02-06 18:02:21 +0530

    Sorted the rows in allotBatch in ASC order

--- dashboard/allotBatch.php
+++ dashboard/allotBatch.php
@@ -37,7 +37,7 @@ if(!isset($_SESSION['userId'])){
 			<?php
 			//----- PostGRE SQL Commands -----
 			// $query = "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1 AND Batch=$2";
-			$result = pg_query_params($dbconn,"SELECT * FROM $tablename_IMac WHERE \"Status\"=$1 AND \"Batch\"=$2",array('PENDING','0'));
+			$result = pg_query_params($dbconn,"SELECT * FROM $tablename_IMac WHERE \"Status\"=$1 AND \"Batch\"=$2 ORDER BY \"Year\ ASC, \"Branch\" ASC,array('PENDING','0'));
 			if (pg_result_status($result)==2) {
 				echo "<tr>
 						<td></td>
