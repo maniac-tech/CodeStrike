4e7fec90b627de22b525e8d9358422af7112b806
maniac-tech <ajj_2396@yahoo.com>
2018-02-06 10:21:29 +0530

    patched the search bar issue, the patch is only functional for allotBatch page

--- dashboard/index.php
+++ dashboard/index.php
@@ -67,7 +67,7 @@ function myFunction() {
   var input, filter, table, tr, td, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
+  table = document.getElementById("willAllotTaskForm");
   tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
