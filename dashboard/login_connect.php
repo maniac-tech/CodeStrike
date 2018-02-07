e24b3364d55f6dace10665cc48ee06213d4fd176
maniac-tech <ajj_2396@yahoo.com>
2018-02-08 00:33:05 +0530

    removed variable name error

--- dashboard/login_connect.php
+++ dashboard/login_connect.php
@@ -8,7 +8,7 @@ $tableName=getenv('PostGRE_DB_Users');
 $tablename_IMac=getenv('PostGRE_DB_IMac');
 $tablename_IMac_Coord=getenv('PostGRE_DB_IMac_Co');
 $tableName_IMac_interviews = getenv('PostGRE_DB_IMac_intr');
-$$tablename_IMac_2018 = getenv('PostGRE_DB_IMac_2018');
+$tablename_IMac_2018 = getenv('PostGRE_DB_IMac_2018');
 
 $dbconn = pg_connect("host=$servername dbname=$databaseName user=$username password=$password");
 if (!$dbconn){
