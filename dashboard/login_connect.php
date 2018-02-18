e73f013337c695146583b5d221c349773cf84e69
maniac-tech <ajj_2396@yahoo.com>
2018-02-18 15:48:40 +0530

    Added hacking db

--- dashboard/login_connect.php
+++ dashboard/login_connect.php
@@ -10,6 +10,7 @@ $tablename_IMac_Coord=getenv('PostGRE_DB_IMac_Co');
 $tableName_IMac_interviews = getenv('PostGRE_DB_IMac_intr');
 $tablename_IMac_2018 = getenv('PostGRE_DB_IMac_2018');
 $tablename_JS_2018 = getenv('PostGRE_DB_JavascriptWorkshop_2018');
+$tablename_Hacking_2018 = getenv('PostGRE_DB_HackingWorkshop_2018');
 
 $dbconn = pg_connect("host=$servername dbname=$databaseName user=$username password=$password");
 if (!$dbconn){
