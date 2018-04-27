4f9352d3122ca85e21bfb428336fad669840a175
maniac-tech <ajj_2396@yahoo.com>
2018-04-28 01:21:26 +0530

    Replaced the HTML code with PHP code, creating dynamic profiles

--- dashboard/coordinators.php
+++ dashboard/coordinators.php
@@ -6,45 +6,30 @@ if(!isset($_SESSION['userId'])){
 }
 ?>
 <div id="content_table">
-	<div class="card">
-		<img src="team2.jpg" alt="John" style="width:100%">
-		<h1>John Doe</h1>
-		<p class="title">CEO & Founder, Example</p>
-		<p>Harvard University</p>
-		<div style="margin: 24px 0;">
-			<a href="#"><i class="fa fa-dribbble"></i></a> 
-			<a href="#"><i class="fa fa-twitter"></i></a>  
-			<a href="#"><i class="fa fa-linkedin"></i></a>  
-			<a href="#"><i class="fa fa-facebook"></i></a> 
-		</div>
-		<p><button>Contact</button></p>
-	</div>	
+	<?php 
+		//----- PostGRE SQL Commands -----
+		// Printing 2018 Data:
+		$query = "SELECT * FROM $tablename_IMac_Coord";
+		$result = pg_query($dbconn, $query);
 
-	<div class="card">
-		<img src="team2.jpg" alt="John" style="width:100%">
-		<h1>John Doe</h1>
-		<p class="title">CEO & Founder, Example</p>
-		<p>Harvard University</p>
-		<div style="margin: 24px 0;">
-			<a href="#"><i class="fa fa-dribbble"></i></a> 
-			<a href="#"><i class="fa fa-twitter"></i></a>  
-			<a href="#"><i class="fa fa-linkedin"></i></a>  
-			<a href="#"><i class="fa fa-facebook"></i></a> 
-		</div>
-		<p><button>Contact</button></p>
-	</div>	
-
-	<div class="card">
-		<img src="team2.jpg" alt="John" style="width:100%">
-		<h1>John Doe</h1>
-		<p class="title">CEO & Founder, Example</p>
-		<p>Harvard University</p>
-		<div style="margin: 24px 0;">
-			<a href="#"><i class="fa fa-dribbble"></i></a> 
-			<a href="#"><i class="fa fa-twitter"></i></a>  
-			<a href="#"><i class="fa fa-linkedin"></i></a>  
-			<a href="#"><i class="fa fa-facebook"></i></a> 
-		</div>
-		<p><button>Contact</button></p>
-	</div>	
+		if (pg_result_status($result)==2) {
+			while ($row = pg_fetch_assoc($result)){
+			echo "<div class=\"card\">";
+			echo "<img src=\"profile.jpg\" alt=\"John\" style=\"width:100%\">";
+			echo "<h1>".$row["Name"]."</h1>";
+			echo "<p class=\"title\">".$row["Year"]." - ".$row["Branch"]."</p>
+			<p>".$row["Email ID"]."</p>
+			<p>".$row["Mobile"]."</p>";
+			echo "
+				<div style=\"margin: 24px 0;\">
+					<a href=\"#\"><i class=\"fa fa-twitter\"></i></a>  
+					<a href=\"#\"><i class=\"fa fa-linkedin\"></i></a>  
+					<a href=\"#\"><i class=\"fa fa-facebook\"></i></a> 
+				</div>";
+			echo "
+			<p><button>Contact</button></p>
+			</div>";	
+			}
+		}
+	 ?>
 </div>
