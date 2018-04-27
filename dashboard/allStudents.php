3be5cd8dd6dc520f005388942f175b4b63738cd1
maniac-tech <ajj_2396@yahoo.com>
2018-04-28 01:19:38 +0530

    Added pending students DB

--- dashboard/allStudents.php
+++ dashboard/allStudents.php
@@ -97,82 +97,6 @@ if(!isset($_SESSION['userId'])){
 		</table>
 
 		<table  id="myTablePending">
-		<tr>
-			<td>Name</td>
-			<td>Year</td>
-			<td>Branch</td>
-			<td>Email</td>
-			<td>Mobile</td>
-			<!-- TO-DO: Print the Batch no alloted from the respective table -->
-			<!-- <th>Batch</th> -->
-		</tr>
-		<?php
-		//----- PostGRE SQL Commands -----
-		// Printing 2018 Data:
-		$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1",array('PENDING'));
-		if (pg_result_status($result)==2) {
-			while($row = pg_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$row["Name"]."</td>";
-				echo "<td>".$row["Year"]."</td>";
-				echo "<td>".$row["Branch"]."</td>";
-				echo "<td>".$row["Email"]."</td>";
-				echo "<td>".$row["Mobile"]."</td>";
-				// echo "<td>".$row["Status"]."</td>";
-				// echo "<td>".$row["Batch"]."</td>";
-				echo "</tr>";
-			}
-		}
-		else{
-			echo "Query Failed.";
-		}
-
-		// Printing 2017 Data:
-		// $query = "SELECT * FROM $tablename_IMac WHERE \"Status\"='$1'";
-		$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1",array('PENDING'));
-		if (pg_result_status($result)==2) {
-			while($row = pg_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$row["Name"]."</td>";
-				echo "<td>".$row["Year"]."</td>";
-				echo "<td>".$row["Branch"]."</td>";
-				echo "<td>".$row["Email"]."</td>";
-				echo "<td>".$row["Mobile"]."</td>";
-				// echo "<td>".$row["Status"]."</td>";
-				// echo "<td>".$row["Batch"]."</td>";
-				echo "</tr>";
-			}
-		}
-		else{
-			echo "Query Failed.";
-		}
-		// -X-X-X- End of PostGRE SQL Commands -X-X-X-
-
-		//----- SQL Commands -----
-		/*
-		$sql = "SELECT * FROM $tableName WHERE Status='Pending'";
-		$result = mysqli_query($conn,$sql);
-		if(mysqli_num_rows($result)>0){
-			while ($row=mysqli_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$row["Name"]."</td>";
-				echo "<td>".$row["Year"]."</td>";
-				echo "<td>".$row["Branch"]."</td>";
-				echo "<td>".$row["Email"]."</td>";
-				echo "<td>".$row["Mobile"]."</td>";
-				echo "<td>".$row["Status"]."</td>";
-				echo "<td>".$row["Batch"]."</td>";
-				echo "</tr>";
-			}
-		}else{
-			echo "NO DATA";
-		}
-		*/
-		// -X-X-X- End of SQL Commands -X-X-X-
-		?>
-	</table>
-
-		<table id="myTableCompleted">
 			<tr>
 				<td>Name</td>
 				<td>Year</td>
@@ -183,43 +107,119 @@ if(!isset($_SESSION['userId'])){
 				<!-- <th>Batch</th> -->
 			</tr>
 			<?php
-			//----- PostGRE SQL Commands -----
-			// Printing 2018 Data:
-			$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1",array('COMPLETED'));
-			if (pg_result_status($result)==2) {
-				while($row = pg_fetch_assoc($result)){
-					echo "<tr>";
-					echo "<td>".$row["Name"]."</td>";
-					echo "<td>".$row["Year"]."</td>";
-					echo "<td>".$row["Branch"]."</td>";
-					echo "<td>".$row["Email"]."</td>";
-					echo "<td>".$row["Mobile"]."</td>";
-							// echo "<td>".$row["Status"]."</td>";
-							// echo "<td>".$row["Batch"]."</td>";
-					echo "</tr>";
-				}
-			}
-			else{
-				echo "Query Failed.";
-			}
-				// Printing 2017 Data:
-			$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1",array('COMPLETED'));
-			if (pg_result_status($result)==2) {
-				while($row = pg_fetch_assoc($result)){
-					echo "<tr>";
-					echo "<td>".$row["Name"]."</td>";
-					echo "<td>".$row["Year"]."</td>";
-					echo "<td>".$row["Branch"]."</td>";
-					echo "<td>".$row["Email"]."</td>";
-					echo "<td>".$row["Mobile"]."</td>";
+				//----- PostGRE SQL Commands -----
+				// Printing 2018 Data:
+					$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1",array('PENDING'));
+					if (pg_result_status($result)==2) {
+						while($row = pg_fetch_assoc($result)){
+							echo "<tr>";
+							echo "<td>".$row["Name"]."</td>";
+							echo "<td>".$row["Year"]."</td>";
+							echo "<td>".$row["Branch"]."</td>";
+							echo "<td>".$row["Email"]."</td>";
+							echo "<td>".$row["Mobile"]."</td>";
 						// echo "<td>".$row["Status"]."</td>";
 						// echo "<td>".$row["Batch"]."</td>";
-					echo "</tr>";
+							echo "</tr>";
+						}
+					}
+					else{
+						echo "Query Failed.";
+					}
+
+				// Printing 2017 Data:
+				// $query = "SELECT * FROM $tablename_IMac WHERE \"Status\"='$1'";
+					$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1",array('PENDING'));
+					if (pg_result_status($result)==2) {
+						while($row = pg_fetch_assoc($result)){
+							echo "<tr>";
+							echo "<td>".$row["Name"]."</td>";
+							echo "<td>".$row["Year"]."</td>";
+							echo "<td>".$row["Branch"]."</td>";
+							echo "<td>".$row["Email"]."</td>";
+							echo "<td>".$row["Mobile"]."</td>";
+						// echo "<td>".$row["Status"]."</td>";
+						// echo "<td>".$row["Batch"]."</td>";
+							echo "</tr>";
+						}
+					}
+					else{
+						echo "Query Failed.";
+					}
+				// -X-X-X- End of PostGRE SQL Commands -X-X-X-
+
+				//----- SQL Commands -----
+				/*
+				$sql = "SELECT * FROM $tableName WHERE Status='Pending'";
+				$result = mysqli_query($conn,$sql);
+				if(mysqli_num_rows($result)>0){
+					while ($row=mysqli_fetch_assoc($result)){
+						echo "<tr>";
+						echo "<td>".$row["Name"]."</td>";
+						echo "<td>".$row["Year"]."</td>";
+						echo "<td>".$row["Branch"]."</td>";
+						echo "<td>".$row["Email"]."</td>";
+						echo "<td>".$row["Mobile"]."</td>";
+						echo "<td>".$row["Status"]."</td>";
+						echo "<td>".$row["Batch"]."</td>";
+						echo "</tr>";
+					}
+				}else{
+					echo "NO DATA";
 				}
+				*/
+				// -X-X-X- End of SQL Commands -X-X-X-
+			?>
+	</table>
+
+	<table id="myTableCompleted">
+		<tr>
+			<td>Name</td>
+			<td>Year</td>
+			<td>Branch</td>
+			<td>Email</td>
+			<td>Mobile</td>
+			<!-- TO-DO: Print the Batch no alloted from the respective table -->
+			<!-- <th>Batch</th> -->
+		</tr>
+		<?php
+			//----- PostGRE SQL Commands -----
+			// Printing 2018 Data:
+		$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1",array('COMPLETED'));
+		if (pg_result_status($result)==2) {
+			while($row = pg_fetch_assoc($result)){
+				echo "<tr>";
+				echo "<td>".$row["Name"]."</td>";
+				echo "<td>".$row["Year"]."</td>";
+				echo "<td>".$row["Branch"]."</td>";
+				echo "<td>".$row["Email"]."</td>";
+				echo "<td>".$row["Mobile"]."</td>";
+							// echo "<td>".$row["Status"]."</td>";
+							// echo "<td>".$row["Batch"]."</td>";
+				echo "</tr>";
 			}
-			else{
-				echo "Query Failed.";
+		}
+		else{
+			echo "Query Failed.";
+		}
+				// Printing 2017 Data:
+		$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1",array('COMPLETED'));
+		if (pg_result_status($result)==2) {
+			while($row = pg_fetch_assoc($result)){
+				echo "<tr>";
+				echo "<td>".$row["Name"]."</td>";
+				echo "<td>".$row["Year"]."</td>";
+				echo "<td>".$row["Branch"]."</td>";
+				echo "<td>".$row["Email"]."</td>";
+				echo "<td>".$row["Mobile"]."</td>";
+						// echo "<td>".$row["Status"]."</td>";
+						// echo "<td>".$row["Batch"]."</td>";
+				echo "</tr>";
 			}
+		}
+		else{
+			echo "Query Failed.";
+		}
 			// -X-X-X- End of PostGRE SQL Commands -X-X-X-
 
 			//----- SQL Commands -----
