604e04c95cb0d2ec2c0ef149648b9a868fb0bfc8
maniac-tech <ajj_2396@yahoo.com>
2018-04-28 21:00:25 +0530

    Added init function for allStudents page

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -3,29 +3,49 @@ document.getElementById("Pending-button").addEventListener("click",loadPendingTa
 document.getElementById("Completed-button").addEventListener("click",loadCompletedTable);
 
 var tableRegister = document.getElementById("myTableRegister");
-	var tablePending = document.getElementById("myTablePending");
-	var tableCompleted = document.getElementById("myTableCompleted");
+var tablePending = document.getElementById("myTablePending");
+var tableCompleted = document.getElementById("myTableCompleted");
+
+function init(){
+	// alert("Page loaded");
+	loadRegisterTable();
+}
 
 function loadRegisterTable(){
-	alert("Register Pressed");	
+	// alert("Register Pressed");	
 	
+	document.getElementById("Register-button").style.backgroundColor = "grey";
+	document.getElementById("Pending-button").style.backgroundColor = "white";
+	document.getElementById("Completed-button").style.backgroundColor = "white";
+
+
 	tableRegister.style.display="block";
 	tablePending.style.display="none";
 	tableCompleted.style.display="none";
 }
 
 function loadPendingTable(){
-	alert("Pending Pressed");	
-	
+	// alert("Pending Pressed");	
+
+	document.getElementById("Register-button").style.backgroundColor = "white";
+	document.getElementById("Pending-button").style.backgroundColor = "grey";
+	document.getElementById("Completed-button").style.backgroundColor = "white";
+
 	tableRegister.style.display="none";
 	tablePending.style.display="block";
 	tableCompleted.style.display="none";
 }
 
 function loadCompletedTable(){
-	alert("Completed Pressed");	
+	// alert("Completed Pressed");	
 	
+	document.getElementById("Register-button").style.backgroundColor = "white";
+	document.getElementById("Pending-button").style.backgroundColor = "white";
+	document.getElementById("Completed-button").style.backgroundColor = "grey";
+
 	tableRegister.style.display="none";
 	tablePending.style.display="none";
 	tableCompleted.style.display="block";
 }
+
+init();
