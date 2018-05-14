cd7e200005d4242f4ecc99597ce02dc8ea900eb2
maniac-tech <ajj_2396@yahoo.com>
2018-05-15 02:09:22 +0530

    adjusted the widths of status page to fit in the divisions

--- dashboard/status.php
+++ dashboard/status.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION['userId'])){
 		xhttp.open("POST","master.php",true);
 		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		// xhttp.send("query=allotTask&Batch="+$("#batchAllotedInput").val());
-			xhttp.send("query=statusComplete&checkbox[]="+checkboxArray);
+		xhttp.send("query=statusComplete&checkbox[]="+checkboxArray);
 	}
 	function loadAjax2(){
 		
@@ -87,8 +87,9 @@ if(!isset($_SESSION['userId'])){
 </script>
 <div id="content_table">
 	<input type="text" id="myInput" onkeyup="myFunctionS()" style="" placeholder="Search for names..">
-
-	<p id="studentsList"><b>List of Students Pending:</b></p>
+	
+	<div id="pendingStudentsList">
+		<p id="studentsList"><b>List of Students Pending:</b></p>
 		<table id="myTable">
 			<tr>
 				<td></td>
@@ -98,78 +99,80 @@ if(!isset($_SESSION['userId'])){
 				<td>Batch</td>
 			</tr>
 			<?php
-			//----- PostGRE SQL Commands -----
-			// Printing 2018 Data:
-			$psql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Batch\"!=0 AND \"Status\"='PENDING'";
-			$result = pg_query($dbconn,$psql);
-			if(pg_num_rows($result)>0){
-				while ($row=pg_fetch_assoc($result)){
-					echo "<tr>";
-					echo "<td><input type='checkbox' name='checkbox[]' value='".$row['Mobile']."'></td>";
-					echo "<td>".$row["Name"]."</td>";
-					echo "<td>".$row["Year"]."</td>";
-					echo "<td>".$row["Branch"]."</td>";
-					echo "<td>".$row["Batch"]."</td>";
-					echo "</tr>";
+				//----- PostGRE SQL Commands -----
+				// Printing 2018 Data:
+				$psql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Batch\"!=0 AND \"Status\"='PENDING'";
+				$result = pg_query($dbconn,$psql);
+				if(pg_num_rows($result)>0){
+					while ($row=pg_fetch_assoc($result)){
+						echo "<tr>";
+						echo "<td><input type='checkbox' name='checkbox[]' value='".$row['Mobile']."'></td>";
+						echo "<td>".$row["Name"]."</td>";
+						echo "<td>".$row["Year"]."</td>";
+						echo "<td>".$row["Branch"]."</td>";
+						echo "<td>".$row["Batch"]."</td>";
+						echo "</tr>";
+					}
+				}else{
+					echo "NO DATA";
 				}
-			}else{
-				echo "NO DATA";
-			}
-			// Printing 2017 Data:
-			$psql = "SELECT * FROM $tablename_IMac WHERE \"Batch\"!=0 AND \"Status\"='PENDING'";
-			$result = pg_query($dbconn,$psql);
-			if(pg_num_rows($result)>0){
-				while ($row=pg_fetch_assoc($result)){
-					echo "<tr>";
-					echo "<td><input type='checkbox' name='checkbox[]' value='".$row['Mobile']."'></td>";
-					echo "<td>".$row["Name"]."</td>";
-					echo "<td>".$row["Year"]."</td>";
-					echo "<td>".$row["Branch"]."</td>";
-					echo "<td>".$row["Batch"]."</td>";
-					echo "</tr>";
+				// Printing 2017 Data:
+				$psql = "SELECT * FROM $tablename_IMac WHERE \"Batch\"!=0 AND \"Status\"='PENDING'";
+				$result = pg_query($dbconn,$psql);
+				if(pg_num_rows($result)>0){
+					while ($row=pg_fetch_assoc($result)){
+						echo "<tr>";
+						echo "<td><input type='checkbox' name='checkbox[]' value='".$row['Mobile']."'></td>";
+						echo "<td>".$row["Name"]."</td>";
+						echo "<td>".$row["Year"]."</td>";
+						echo "<td>".$row["Branch"]."</td>";
+						echo "<td>".$row["Batch"]."</td>";
+						echo "</tr>";
+					}
+				}else{
+					echo "NO DATA";
 				}
-			}else{
-				echo "NO DATA";
-			}
-			// -X-X-X- End of PostGRE SQL Commands -X-X-X-
+				// -X-X-X- End of PostGRE SQL Commands -X-X-X-
 
-			//----- SQL Commands -----
-			/*
-			$sql = "SELECT * FROM $tableName WHERE Batch!=0 AND Status='Pending'";
-			$result = mysqli_query($conn,$sql);
-			if(mysqli_num_rows($result)>0){
-				while ($row=mysqli_fetch_assoc($result)){
-					echo "<tr>";
-					echo "<td><input type='checkbox' name='checkbox[]' value='".$row['Mobile']."'></td>";
-					echo "<td>".$row["Name"]."</td>";
-					echo "<td>".$row["Year"]."</td>";
-					echo "<td>".$row["Branch"]."</td>";
-					echo "<td>".$row["Batch"]."</td>";
-					echo "</tr>";
+				//----- SQL Commands -----
+				/*
+				$sql = "SELECT * FROM $tableName WHERE Batch!=0 AND Status='Pending'";
+				$result = mysqli_query($conn,$sql);
+				if(mysqli_num_rows($result)>0){
+					while ($row=mysqli_fetch_assoc($result)){
+						echo "<tr>";
+						echo "<td><input type='checkbox' name='checkbox[]' value='".$row['Mobile']."'></td>";
+						echo "<td>".$row["Name"]."</td>";
+						echo "<td>".$row["Year"]."</td>";
+						echo "<td>".$row["Branch"]."</td>";
+						echo "<td>".$row["Batch"]."</td>";
+						echo "</tr>";
+					}
+				}else{
+					echo "NO DATA";
 				}
-			}else{
-				echo "NO DATA";
-			}
-			*/
-			// -X-X-X- End of SQL Commands -X-X-X-
+				*/
+				// -X-X-X- End of SQL Commands -X-X-X-
 			?>
 		</table>
-</div>
-<div id="operations">
-	<p id="studentsList">About:</p>
-	<p>
-		This section is used to change the status of the Students, after they have completed their training.
-		
-		Instructions:
-		<ol>
-			<li>Select Students to change Status.</li>
-			<li>Click on <b>Complete Button</b> below, to change the status.</li>
-		</ol>
-	</p>
-	<p>
-		<button onclick="loadAjax()">Completed</button>
-		<button onclick="loadAjax2()">Pending</button>
-		<button onclick="loadAjax3()">Not Attended</button>
-		<button onclick="loadAjax4()">Incomplete</button>
-	</p>
+	</div>
+
+	<div id="operations">
+		<p id="studentsList">About:</p>
+		<p>
+			This section is used to change the status of the Students, after they have completed their training.
+			
+			Instructions:
+			<ol>
+				<li>Select Students to change Status.</li>
+				<li>Click on <b>Complete Button</b> below, to change the status.</li>
+			</ol>
+		</p>
+		<p>
+			<button onclick="loadAjax()">Completed</button>
+			<button onclick="loadAjax2()">Pending</button>
+			<button onclick="loadAjax3()">Not Attended</button>
+			<button onclick="loadAjax4()">Incomplete</button>
+		</p>
+	</div>
 </div>
