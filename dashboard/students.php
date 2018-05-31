3cab38d3aef4f064b4bb023a2106c5e5373934ec
maniac-tech <ajj_2396@yahoo.com>
2018-06-01 01:25:43 +0530

    Removed Export button from the bottom of the page, and added the same functionality to the Download data button

--- dashboard/students.php
+++ dashboard/students.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['userId'])){
 		<button id="Completed-button"><a href="pendingStudents">Completed</a></button>
 		<button id="Pending-button"><a href="pendingStudents">Pending</a></button>
 		<button id="Register-button"><a href="#registeredStudents">Registered</a></button>
-		<button id="download-data-button"><img src="download_icon.png" alt="" id="download-icon" >Download Data</button>
+		<button id="download-data-button" onclick="downloadData()"><img src="download_icon.png" alt="" id="download-icon" >Download Data</button>
 	</div>
 	<br>
 
@@ -112,43 +112,43 @@ if(!isset($_SESSION['userId'])){
 			<?php
 				//----- PostGRE SQL Commands -----
 				// Printing 2018 Data:
-					$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1",array('PENDING'));
-					if (pg_result_status($result)==2) {
-						while($row = pg_fetch_assoc($result)){
-							echo "<tr>";
-							echo "<td>".$row["Name"]."</td>";
-							echo "<td>".$row["Year"]."</td>";
-							echo "<td>".$row["Branch"]."</td>";
-							echo "<td>".$row["Email"]."</td>";
-							echo "<td>".$row["Mobile"]."</td>";
+			$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1",array('PENDING'));
+			if (pg_result_status($result)==2) {
+				while($row = pg_fetch_assoc($result)){
+					echo "<tr>";
+					echo "<td>".$row["Name"]."</td>";
+					echo "<td>".$row["Year"]."</td>";
+					echo "<td>".$row["Branch"]."</td>";
+					echo "<td>".$row["Email"]."</td>";
+					echo "<td>".$row["Mobile"]."</td>";
 						// echo "<td>".$row["Status"]."</td>";
 						// echo "<td>".$row["Batch"]."</td>";
-							echo "</tr>";
-						}
-					}
-					else{
-						echo "Query Failed.";
-					}
+					echo "</tr>";
+				}
+			}
+			else{
+				echo "Query Failed.";
+			}
 
 				// Printing 2017 Data:
 				// $query = "SELECT * FROM $tablename_IMac WHERE \"Status\"='$1'";
-					$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1",array('PENDING'));
-					if (pg_result_status($result)==2) {
-						while($row = pg_fetch_assoc($result)){
-							echo "<tr>";
-							echo "<td>".$row["Name"]."</td>";
-							echo "<td>".$row["Year"]."</td>";
-							echo "<td>".$row["Branch"]."</td>";
-							echo "<td>".$row["Email"]."</td>";
-							echo "<td>".$row["Mobile"]."</td>";
+			$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1",array('PENDING'));
+			if (pg_result_status($result)==2) {
+				while($row = pg_fetch_assoc($result)){
+					echo "<tr>";
+					echo "<td>".$row["Name"]."</td>";
+					echo "<td>".$row["Year"]."</td>";
+					echo "<td>".$row["Branch"]."</td>";
+					echo "<td>".$row["Email"]."</td>";
+					echo "<td>".$row["Mobile"]."</td>";
 						// echo "<td>".$row["Status"]."</td>";
 						// echo "<td>".$row["Batch"]."</td>";
-							echo "</tr>";
-						}
-					}
-					else{
-						echo "Query Failed.";
-					}
+					echo "</tr>";
+				}
+			}
+			else{
+				echo "Query Failed.";
+			}
 				// -X-X-X- End of PostGRE SQL Commands -X-X-X-
 
 				//----- SQL Commands -----
@@ -172,97 +172,109 @@ if(!isset($_SESSION['userId'])){
 				}
 				*/
 				// -X-X-X- End of SQL Commands -X-X-X-
-			?>
-	</table>
+				?>
+			</table>
 
-	<table id="myTableCompleted">
-		<tr>
-			<td>Name</td>
-			<td>Year</td>
-			<td>Branch</td>
-			<td>Email</td>
-			<td>Mobile</td>
-			<!-- TO-DO: Print the Batch no alloted from the respective table -->
-			<!-- <th>Batch</th> -->
-		</tr>
-		<?php
-			//----- PostGRE SQL Commands -----
-			// Printing 2018 Data:
-		$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1",array('COMPLETED'));
-		if (pg_result_status($result)==2) {
-			while($row = pg_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$row["Name"]."</td>";
-				echo "<td>".$row["Year"]."</td>";
-				echo "<td>".$row["Branch"]."</td>";
-				echo "<td>".$row["Email"]."</td>";
-				echo "<td>".$row["Mobile"]."</td>";
-							// echo "<td>".$row["Status"]."</td>";
-							// echo "<td>".$row["Batch"]."</td>";
-				echo "</tr>";
-			}
-		}
-		else{
-			echo "Query Failed.";
-		}
-				// Printing 2017 Data:
-		$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1",array('COMPLETED'));
-		if (pg_result_status($result)==2) {
-			while($row = pg_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$row["Name"]."</td>";
-				echo "<td>".$row["Year"]."</td>";
-				echo "<td>".$row["Branch"]."</td>";
-				echo "<td>".$row["Email"]."</td>";
-				echo "<td>".$row["Mobile"]."</td>";
-						// echo "<td>".$row["Status"]."</td>";
-						// echo "<td>".$row["Batch"]."</td>";
-				echo "</tr>";
-			}
-		}
-		else{
-			echo "Query Failed.";
-		}
-			// -X-X-X- End of PostGRE SQL Commands -X-X-X-
-
-			//----- SQL Commands -----
-			/*
-			$sql = "SELECT * FROM $tableName WHERE Status='Completed'";
-			$result = mysqli_query($conn,$sql);
-			if(mysqli_num_rows($result)>0){
-				while ($row=mysqli_fetch_assoc($result)){
-					echo "<tr>";
-					echo "<td>".$row["Name"]."</td>";
-					echo "<td>".$row["Year"]."</td>";
-					echo "<td>".$row["Branch"]."</td>";
-					echo "<td>".$row["Email"]."</td>";
-					echo "<td>".$row["Mobile"]."</td>";
-					echo "<td>".$row["Status"]."</td>";
-					echo "</tr>";
+			<table id="myTableCompleted">
+				<tr>
+					<td>Name</td>
+					<td>Year</td>
+					<td>Branch</td>
+					<td>Email</td>
+					<td>Mobile</td>
+					<!-- TO-DO: Print the Batch no alloted from the respective table -->
+					<!-- <th>Batch</th> -->
+				</tr>
+				<?php
+				//----- PostGRE SQL Commands -----
+				// Printing 2018 Data:
+				$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1",array('COMPLETED'));
+				if (pg_result_status($result)==2) {
+					while($row = pg_fetch_assoc($result)){
+						echo "<tr>";
+						echo "<td>".$row["Name"]."</td>";
+						echo "<td>".$row["Year"]."</td>";
+						echo "<td>".$row["Branch"]."</td>";
+						echo "<td>".$row["Email"]."</td>";
+						echo "<td>".$row["Mobile"]."</td>";
+									// echo "<td>".$row["Status"]."</td>";
+									// echo "<td>".$row["Batch"]."</td>";
+						echo "</tr>";
+					}
 				}
-			}else{
-				echo "NO DATA";
-			}
-			*/
-			// -X-X-X- End of SQL Commands -X-X-X-
-			?>
-		</table>
-	</div>
-	<button onclick="$('#myTable').tableExport({
-		headers: true,                              // (Boolean), display table headers (th or td elements) in the <thead>, (default: true)
-	    footers: true,                              // (Boolean), display table footers (th or td elements) in the <tfoot>, (default: false)
-	    formats: ['xls', 'csv', 'txt'],             // (String[]), filetype(s) for the export, (default: ['xls', 'csv', 'txt'])
-    	filename: 'All Students',                             // (id, String), filename for the downloaded file, (default: 'id')
-   		bootstrap: true,                           // (Boolean), style buttons using bootstrap, (default: true)
-    	exportButtons: true,                        // (Boolean), automatically generate the built-in export buttons for each of the specified formats (default: true)
-    	position: 'bottom',                         // (top, bottom), position of the caption element relative to table, (default: 'bottom')
-    	ignoreRows: null,                           // (Number, Number[]), row indices to exclude from the exported file(s) (default: null)
-    	ignoreCols: null,                           // (Number, Number[]), column indices to exclude from the exported file(s) (default: null)
-    	trimWhitespace: true,
-    	// type: 'csv' 
-    }
-    );">
-    Export
-</button>
+				else{
+					echo "Query Failed.";
+				}
+						// Printing 2017 Data:
+				$result = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Status\"=$1",array('COMPLETED'));
+				if (pg_result_status($result)==2) {
+					while($row = pg_fetch_assoc($result)){
+						echo "<tr>";
+						echo "<td>".$row["Name"]."</td>";
+						echo "<td>".$row["Year"]."</td>";
+						echo "<td>".$row["Branch"]."</td>";
+						echo "<td>".$row["Email"]."</td>";
+						echo "<td>".$row["Mobile"]."</td>";
+								// echo "<td>".$row["Status"]."</td>";
+								// echo "<td>".$row["Batch"]."</td>";
+						echo "</tr>";
+					}
+				}
+				else{
+					echo "Query Failed.";
+				}
+				// -X-X-X- End of PostGRE SQL Commands -X-X-X-
+
+				//----- SQL Commands -----
+				/*
+				$sql = "SELECT * FROM $tableName WHERE Status='Completed'";
+				$result = mysqli_query($conn,$sql);
+				if(mysqli_num_rows($result)>0){
+					while ($row=mysqli_fetch_assoc($result)){
+						echo "<tr>";
+						echo "<td>".$row["Name"]."</td>";
+						echo "<td>".$row["Year"]."</td>";
+						echo "<td>".$row["Branch"]."</td>";
+						echo "<td>".$row["Email"]."</td>";
+						echo "<td>".$row["Mobile"]."</td>";
+						echo "<td>".$row["Status"]."</td>";
+						echo "</tr>";
+					}
+				}else{
+					echo "NO DATA";
+				}
+				*/
+				// -X-X-X- End of SQL Commands -X-X-X-
+				?>
+			</table>
+		</div>
+
 <script src="js/index.js"></script>
+<script>
+	function downloadData(){
+		$('#tab-data').tableExport({
+		// (Boolean), display table headers (th or td elements) in the <thead>, (default: true)
+		headers: true, 
+	    // (Boolean), display table footers (th or td elements) in the <tfoot>, (default: false)
+	    footers: true, 
+	    // (String[]), filetype(s) for the export, (default: ['xls', 'csv', 'txt'])
+	    formats: ['xls', 'csv', 'txt'], 
+    	// (id, String), filename for the downloaded file, (default: 'id')
+    	filename: 'All Students', 
+   		// (Boolean), style buttons using bootstrap, (default: true)
+   		bootstrap: true, 
+    	// (Boolean), automatically generate the built-in export buttons for each of the specified formats (default: true)
+    	exportButtons: true, 
+    	// (top, bottom), position of the caption element relative to table, (default: 'bottom')
+    	position: 'bottom', 
+    	// (Number, Number[]), row indices to exclude from the exported file(s) (default: null)
+    	ignoreRows: null, 
+    	// (Number, Number[]), column indices to exclude from the exported file(s) (default: null)
+    	ignoreCols: null, 
+    	// type: 'csv' 
+    	trimWhitespace: true,
+    }
+    );
+	}
+</script>
 </div>
