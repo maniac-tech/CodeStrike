48942017bc68eefa27c621e7ab26eeb24f9a8608
maniac-tech <ajj_2396@yahoo.com>
2018-06-11 00:54:15 +0530

    Added Canvas JS to populate Doughnut graph

--- dashboard/overview.php
+++ dashboard/overview.php
@@ -1,55 +1,85 @@
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <?php
 
 	//----- PostGRE SQL Commands -----	
-	$sql="";
-	$result="";
-	$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='CMPN' ";
-	$result = pg_query($dbconn, $sql);
-	$countCMPN = pg_num_rows($result);
+$sql="";
+$result="";
+$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='CMPN' ";
+$result = pg_query($dbconn, $sql);
+$countCMPN = pg_num_rows($result);
 	// echo "$countCMPN";
 
-	$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='IT' ";
-	$result = pg_query($dbconn, $sql);
-	$countIT = pg_num_rows($result);
+$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='IT' ";
+$result = pg_query($dbconn, $sql);
+$countIT = pg_num_rows($result);
 
-	$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='EXTC' ";
-	$result = pg_query($dbconn, $sql);
-	$countEXTC = pg_num_rows($result);
+$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='EXTC' ";
+$result = pg_query($dbconn, $sql);
+$countEXTC = pg_num_rows($result);
 
-	$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='ELEC' ";
-	$result = pg_query($dbconn, $sql);
-	$countELEC = pg_num_rows($result);
+$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='ELEC' ";
+$result = pg_query($dbconn, $sql);
+$countELEC = pg_num_rows($result);
 
-	$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='ELEX' ";
-	$result = pg_query($dbconn, $sql);
-	$countELEX = pg_num_rows($result);
+$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Branch\"='ELEX' ";
+$result = pg_query($dbconn, $sql);
+$countELEX = pg_num_rows($result);
 
-	$sql = "SELECT  * FROM $tablename_IMac_2018 WHERE \"Year\"='BE' ";
-	$result = pg_query($dbconn,$sql);
-	$countBE = pg_num_rows($result);
+$sql = "SELECT  * FROM $tablename_IMac_2018 WHERE \"Year\"='BE' ";
+$result = pg_query($dbconn,$sql);
+$countBE = pg_num_rows($result);
 
-	$sql = "SELECT  * FROM $tablename_IMac_2018 WHERE \"Year\"='TE' ";
-	$result = pg_query($dbconn,$sql);
-	$countTE = pg_num_rows($result);
+$sql = "SELECT  * FROM $tablename_IMac_2018 WHERE \"Year\"='TE' ";
+$result = pg_query($dbconn,$sql);
+$countTE = pg_num_rows($result);
 
-	$sql = "SELECT  * FROM $tablename_IMac_2018 WHERE \"Year\"='SE' ";
-	$result = pg_query($dbconn,$sql);
-	$countSE = pg_num_rows($result);	
+$sql = "SELECT  * FROM $tablename_IMac_2018 WHERE \"Year\"='SE' ";
+$result = pg_query($dbconn,$sql);
+$countSE = pg_num_rows($result);	
 
-	$total_reg = $countIT+$countCMPN+$countELEX+$countELEC+$countEXTC;
+$total_reg = $countIT+$countCMPN+$countELEX+$countELEC+$countEXTC;
 
 
-	$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"='PENDING' ";
-	$result = pg_query($dbconn, $sql);
-	$countPending = pg_num_rows($result);
+$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"='PENDING' ";
+$result = pg_query($dbconn, $sql);
+$countPending = pg_num_rows($result);
 
-	$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"='COMPLETED' ";
-	$result = pg_query($dbconn, $sql);
-	$countCompleted = pg_num_rows($result);
+$sql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"='COMPLETED' ";
+$result = pg_query($dbconn, $sql);
+$countCompleted = pg_num_rows($result);
 	// -X-X-X- End of PostGRE SQL Commands -X-X-X-
 
 ?>
+<script>
+	window.onload = function () {
+
+		var chart = new CanvasJS.Chart("branch-wise", {
+			animationEnabled: true,
+			title:{
+				text: "Branch-wise Data",
+				horizontalAlign: "left"
+			},
+			data: [{
+				type: "doughnut",
+				startAngle: 60,
+		//innerRadius: 60,
+		indexLabelFontSize: 17,
+		indexLabel: "{label} - #percent%",
+		toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+		dataPoints: [
+		{ y: <?php echo $countCMPN ?>, label: "CMPN" },
+		{ y: <?php echo $countIT ?>, label: "INFT" },
+		{ y: <?php echo $countELEX ?>, label: "ELEX" },
+		{ y: <?php echo $countELEC ?>, label: "ELEC"},
+		{ y: <?php echo $countEXTC ?>, label: "EXTC"},
+		]
+	}]
+});
+		chart.render();
+
+	}
+</script>
+<script src="js/canvasjs/canvasjs.min.js"></script>
+
 <div id="top_charts">
 	<div id="total_reg">
 		<center>		
@@ -60,17 +90,17 @@
 	</div>
 	<div id="complete_reg">
 		<center>
-		<p id="top_charts_val"><?php echo $countCompleted; ?></p>
-		<br>
-		<p id="top_charts_name">Completed Students</p>
-	</center>
+			<p id="top_charts_val"><?php echo $countCompleted; ?></p>
+			<br>
+			<p id="top_charts_name">Completed Students</p>
+		</center>
 	</div>
 	<div id="pending_reg">
 		<center>
-		<p id="top_charts_val"><?php echo $countPending; ?></p>
-		<br>
-		<p id="top_charts_name">Pending Students</p>
-	</center>
+			<p id="top_charts_val"><?php echo $countPending; ?></p>
+			<br>
+			<p id="top_charts_name">Pending Students</p>
+		</center>
 	</div>
 </div>
 <div id="bottom_charts">	
