583c13bfea95c0faba068d27f8cb43770bfae2fb
maniac-tech <ajj_2396@yahoo.com>
2018-06-15 00:19:12 +0530

    Removed empty Operations Division

--- dashboard/batch.php
+++ dashboard/batch.php
@@ -44,8 +44,8 @@ if(!isset($_SESSION['userId'])){
 			<form name="willAllotBatchForm" action="">
 				<table id="myTableAllotBatch_details">
 					<?php
-						//----- PostGRE SQL Commands -----
-						// Printing 2018 Data:
+					//----- PostGRE SQL Commands -----
+					// Printing 2018 Data:
 					$result = pg_query_params($dbconn,"SELECT * FROM $tablename_IMac_2018 WHERE \"Status\"=$1 AND \"Batch\"=$2 ",array('PENDING','0'));
 					if (pg_result_status($result)==2) {
 						echo "<tr>
@@ -60,10 +60,10 @@ if(!isset($_SESSION['userId'])){
 							echo "<td>".$row["Name"]."</td>";
 							echo "<td>".$row["Year"]."</td>";
 							echo "<td>".$row["Branch"]."</td>";
-									// echo "<td>".$row["Email"]."</td>";
-									// echo "<td>".$row["Mobile"]."</td>";
-									// echo "<td>".$row["Status"]."</td>";
-									// echo "<td>".$row["Batch"]."</td>";
+								// echo "<td>".$row["Email"]."</td>";
+								// echo "<td>".$row["Mobile"]."</td>";
+								// echo "<td>".$row["Status"]."</td>";
+								// echo "<td>".$row["Batch"]."</td>";
 							echo "</tr>";
 						}
 					}
@@ -71,7 +71,7 @@ if(!isset($_SESSION['userId'])){
 						echo "Query Failed.";
 						echo pg_result_status($result);
 					}
-							// Printing 2017 Data:
+						// Printing 2017 Data:
 					$result = pg_query_params($dbconn,"SELECT * FROM $tablename_IMac WHERE \"Status\"=$1 AND \"Batch\"=$2 ",array('PENDING','0'));
 					if (pg_result_status($result)==2) {
 						echo "<tr>
@@ -86,10 +86,10 @@ if(!isset($_SESSION['userId'])){
 							echo "<td>".$row["Name"]."</td>";
 							echo "<td>".$row["Year"]."</td>";
 							echo "<td>".$row["Branch"]."</td>";
-									// echo "<td>".$row["Email"]."</td>";
-									// echo "<td>".$row["Mobile"]."</td>";
-									// echo "<td>".$row["Status"]."</td>";
-									// echo "<td>".$row["Batch"]."</td>";
+								// echo "<td>".$row["Email"]."</td>";
+								// echo "<td>".$row["Mobile"]."</td>";
+								// echo "<td>".$row["Status"]."</td>";
+								// echo "<td>".$row["Batch"]."</td>";
 							echo "</tr>";
 						}
 					}
@@ -102,12 +102,10 @@ if(!isset($_SESSION['userId'])){
 				</table>
 			</form>
 			<div id="myTableAllotBatch_operations">
-				<p id="allotBatch_header">
-					Allot Batch:
-				</p>
+				<p id="allotBatch_header">Allot Batch:</p>
 				<p id="allotBatch_content">
 					<p>
-					Instructions:
+						Instructions:
 						<ol>
 							<li>Select Students to allot batch.</li>
 							<li>Enter their Batch no in the field Below.</li>
@@ -115,93 +113,95 @@ if(!isset($_SESSION['userId'])){
 						</ol>
 					</p>
 				</p>
-				<!-- <form action="" id="allotBatch_form" > -->
-					<p>Enter Batch No:</p>
-					<input type="text" form="willAllotBatchForm" id="batchAllotedInput" required>
-					<button onclick="loadAjax()">Allot Batch</button>
-					<p>Total no. of students selected:</p>
-					<!-- </form> -->
-				</div>
+				<p>Enter Batch No:</p>
+				<input type="text" form="willAllotBatchForm" id="batchAllotedInput" required>
+				<button onclick="loadAjax()">Allot Batch</button>
+				<p>Total no. of students selected:</p>
 			</div>
-			<table id="myTableOperations">
-				<tr><td>DEFINE USE OF THIS BUTTON</td></tr>
-			</table>
-			<table id="myTableBatchInfo">
-				<?php 
-				//----- PostGRE SQL Commands -----
-				// Printing 2018 Data:
-				$query = "SELECT * FROM $tablename_IMac_2018_Batch";
-				$result = pg_query($dbconn, $query);
-				if (pg_result_status($result)==2){
-					while($row = pg_fetch_assoc($result)){
-						echo"<button class=\"accordion\">Batch No: ".$row['Batch No']."     Total Students: ".$row['Total Students']."/20</button>
-						<div class=\"panel\">";
-						$result2 = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Batch\"=$1",array($row['Batch No']));
-						$result3 = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Batch\"=$1",array($row['Batch No']));
-						if (pg_result_status($result2)==2){
-							echo "<table border=1>
-							<tr>
-							<td>Name</td>
-							<td>Year</td>
-							<td>Branch</td>
-							</tr>
-							";
-							while($row2 = pg_fetch_assoc($result2)){
-								echo "<tr>
-								<td>".$row2['Name']."</td>
-								<td>".$row2['Year']."</td>
-								<td>".$row2['Branch']."</td>
-								</tr>";
-							}
-							while($row3 = pg_fetch_assoc($result3)){
-								echo "<tr>
-								<td>".$row3['Name']."</td>
-								<td>".$row3['Year']."</td>
-								<td>".$row3['Branch']."</td>
-								</tr>";
-							}
-							echo "</table>";
+		</div>
+		<table id="myTableOperations">
+			<tr>
+				<td>DEFINE USE OF THIS BUTTON</td>
+			</tr>
+		</table>
+		<table id="myTableBatchInfo">
+			<?php 
+			//----- PostGRE SQL Commands -----
+			// Printing 2018 Data:
+			$query = "SELECT * FROM $tablename_IMac_2018_Batch";
+			$result = pg_query($dbconn, $query);
+			if (pg_result_status($result)==2){
+				while($row = pg_fetch_assoc($result)){
+					echo"<button class=\"accordion\">Batch No: ".$row['Batch No']."     Total Students: ".$row['Total Students']."/20</button>
+					<div class=\"panel\">";
+					$result2 = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac WHERE \"Batch\"=$1",array($row['Batch No']));
+					$result3 = pg_query_params($dbconn, "SELECT * FROM $tablename_IMac_2018 WHERE \"Batch\"=$1",array($row['Batch No']));
+					if (pg_result_status($result2)==2){
+						echo "<table border=1>
+						<th colspan = 4>
+							Atharva iMac Lab Batch:53 
+						</th>
+						<tr>
+						<td>Sr. no</td>
+						<td>Name of Student</td>
+						<td>Year</td>
+						<td>Branch</td>
+						</tr>
+						";
+						while($row2 = pg_fetch_assoc($result2)){
+							echo "<tr>
+							<td></td>
+							<td>".$row2['Name']."</td>
+							<td>".$row2['Year']."</td>
+							<td>".$row2['Branch']."</td>
+							</tr>";
 						}
-						echo "</div>";
+						while($row3 = pg_fetch_assoc($result3)){
+							echo "<tr>
+							<td>".$row3['Name']."</td>
+							<td>".$row3['Year']."</td>
+							<td>".$row3['Branch']."</td>
+							</tr>";
+						}
+						echo "</table>";
 					}
+					echo "</div>";
 				}
-				?>
-			</table>
-		</div>	
-		<form name="willAllotTaskForm" id="willAllotTaskForm">
-			<table id="myTable">
+			}
+			?>
+		</table>
+	</div>	
+	<form name="willAllotTaskForm" id="willAllotTaskForm">
+		<table id="myTable">
+		</table>
+	</form>
+</div>
 
 
-			</table>
-		</form>
-	</div>
-	<div id="operations">
+<style>
+.accordion {
+	background-color: #eee;
+	color: #444;
+	cursor: pointer;
+	padding: 18px;
+	width: 100%;
+	border: none;
+	text-align: left;
+	outline: none;
+	font-size: 15px;
+	transition: 0.4s;
+}
 
-	</div>
-	<style>
-	.accordion {
-		background-color: #eee;
-		color: #444;
-		cursor: pointer;
-		padding: 18px;
-		width: 100%;
-		border: none;
-		text-align: left;
-		outline: none;
-		font-size: 15px;
-		transition: 0.4s;
-	}
+.active, .accordion:hover {
+	background-color: #ccc; 
+}
 
-	.active, .accordion:hover {
-		background-color: #ccc; 
-	}
-
-	.panel {
-		padding: 0 18px;
-		display: none;
-		background-color: white;
-		overflow: hidden;
-	}
+.panel {
+	padding: 0 18px;
+	display: none;
+	background-color: white;
+	overflow: hidden;
+}
 </style>
 <script>
 	var acc = document.getElementsByClassName("accordion");
