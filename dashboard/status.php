b0a23c17089fa2eaa0de3144dbc1fe84bf856f36
maniac-tech <ajj_2396@yahoo.com>
2018-06-15 16:22:35 +0530

    Updated DB variables

--- dashboard/status.php
+++ dashboard/status.php
@@ -100,8 +100,8 @@ if(!isset($_SESSION['userId'])){
 			</tr>
 			<?php
 				//----- PostGRE SQL Commands -----
-				// Printing 2018 Data:
-				$psql = "SELECT * FROM $tablename_IMac_2018 WHERE \"Batch\"!=0 AND \"Status\"='PENDING'";
+				// Printing Data:
+				$psql = "SELECT * FROM $tablename_iMac WHERE \"Batch\"!=0 AND \"Status\"='PENDING' ORDER BY \"Batch\" ASC, \"Name\" ASC";
 				$result = pg_query($dbconn,$psql);
 				if(pg_num_rows($result)>0){
 					while ($row=pg_fetch_assoc($result)){
@@ -116,43 +116,8 @@ if(!isset($_SESSION['userId'])){
 				}else{
 					echo "NO DATA";
 				}
-				// Printing 2017 Data:
-				$psql = "SELECT * FROM $tablename_IMac WHERE \"Batch\"!=0 AND \"Status\"='PENDING'";
-				$result = pg_query($dbconn,$psql);
-				if(pg_num_rows($result)>0){
-					while ($row=pg_fetch_assoc($result)){
-						echo "<tr>";
-						echo "<td><input type='checkbox' name='checkbox[]' value='".$row['Mobile']."'></td>";
-						echo "<td>".$row["Name"]."</td>";
-						echo "<td>".$row["Year"]."</td>";
-						echo "<td>".$row["Branch"]."</td>";
-						echo "<td>".$row["Batch"]."</td>";
-						echo "</tr>";
-					}
-				}else{
-					echo "NO DATA";
-				}
+				
 				// -X-X-X- End of PostGRE SQL Commands -X-X-X-
-
-				//----- SQL Commands -----
-				/*
-				$sql = "SELECT * FROM $tableName WHERE Batch!=0 AND Status='Pending'";
-				$result = mysqli_query($conn,$sql);
-				if(mysqli_num_rows($result)>0){
-					while ($row=mysqli_fetch_assoc($result)){
-						echo "<tr>";
-						echo "<td><input type='checkbox' name='checkbox[]' value='".$row['Mobile']."'></td>";
-						echo "<td>".$row["Name"]."</td>";
-						echo "<td>".$row["Year"]."</td>";
-						echo "<td>".$row["Branch"]."</td>";
-						echo "<td>".$row["Batch"]."</td>";
-						echo "</tr>";
-					}
-				}else{
-					echo "NO DATA";
-				}
-				*/
-				// -X-X-X- End of SQL Commands -X-X-X-
 			?>
 		</table>
 	</div>
