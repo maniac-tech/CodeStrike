713e8e1bd9d66074d8eb1357d6bf2f59e81b6727
maniac-tech <ajj_2396@yahoo.com>
2018-06-15 15:50:41 +0530

    Changed DB Credentials

--- dashboard/login_connect.php
+++ dashboard/login_connect.php
@@ -1,28 +1,21 @@
 <?php
-// Connecting, selecting database
+// Database Credentials:
 $servername=getenv('PostGRE_DB_Host');
 $databaseName=getenv('PostGRE_DB');
 $username=getenv('PostGRE_DB_User');
 $password=getenv('PostGRE_DB_Password');
+
+// Table Variables:
 $tableName=getenv('PostGRE_DB_Users');
-$tablename_IMac=getenv('PostGRE_DB_IMac');
+
+// iMac:
+$tablename_iMac=getenv('PostGRE_DB_imac');
 $tablename_IMac_Coord=getenv('PostGRE_DB_IMac_Co');
 $tableName_IMac_interviews = getenv('PostGRE_DB_IMac_intr');
-$tablename_IMac_2018 = getenv('PostGRE_DB_IMac_2018');
+
+// CodeStrike:
 $tablename_JS_2018 = getenv('PostGRE_DB_JavascriptWorkshop_2018');
 $tablename_Hacking_2018 = getenv('PostGRE_DB_HackingWorkshop_2018');
-
-// $servername="localhost";
-// $databaseName="dashboard";
-// $username="postgres";
-// $password="root";
-// $tableName="dashboard_users";
-// $tablename_IMac="imac2017_registrations";
-// $tablename_IMac_Coord="imac2017_coordinators";
-// $tableName_IMac_interviews = "imac2018_interviews";
-// $tablename_IMac_2018 = "imac2018_registrations_even";
-// $tablename_JS_2018 = getenv('PostGRE_DB_JavascriptWorkshop_2018');
-// $tablename_Hacking_2018 = getenv('PostGRE_DB_HackingWorkshop_2018');
 
 $dbconn = pg_connect("host=$servername dbname=$databaseName user=$username password=$password");
 if (!$dbconn){
