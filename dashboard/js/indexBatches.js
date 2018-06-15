a12a531863ac4e6c13c4cf6bff1c1c5a09ce1d4e
maniac-tech <ajj_2396@yahoo.com>
2018-06-15 16:12:58 +0530

    Added display property to the diff tables

--- dashboard/js/indexBatches.js
+++ dashboard/js/indexBatches.js
@@ -1,31 +1,50 @@
-document.getElementById("Operations-button").addEventListener("click",loadOperationsTable);
-document.getElementById("AllotBatch-button").addEventListener("click",loadAllotBatchTable);
 document.getElementById("BatchInfo-button").addEventListener("click",loadBatchInfoTable);
+document.getElementById("AllotBatch-button").addEventListener("click",loadAllotBatchTable);
+document.getElementById("Operations-button").addEventListener("click",loadOperationsTable);
 
+var tableBatchInfo = document.getElementById("myTableBatchInfo");
 var tableAllotBatch = document.getElementById("myTableAllotBatch");
 var tableOperations = document.getElementById("myTableOperations");
-var tableBatchInfo = document.getElementById("myTableBatchInfo");
 
-function loadOperationsTable(){
-	alert("Operations Pressed");	
+function init(){
+	// alert("Page loaded");
+	loadBatchInfoTable();
+}
+
+function loadBatchInfoTable(){
+	// alert("Batch Info Pressed");	
 	
-	tableOperations.style.display="block";
+	document.getElementById("BatchInfo-button").style.backgroundColor = "grey";
+	document.getElementById("AllotBatch-button").style.backgroundColor = "white";
+	document.getElementById("Operations-button").style.backgroundColor = "white";
+
+	tableOperations.style.display="none";
 	tableAllotBatch.style.display="none";
-	tableBatchInfo.style.display="none";
+	tableBatchInfo.style.display="block";
 }
 
 function loadAllotBatchTable(){
-	alert("Allot Batch Pressed");	
+	// alert("Allot Batch Pressed");	
 	
+	document.getElementById("BatchInfo-button").style.backgroundColor = "white";
+	document.getElementById("AllotBatch-button").style.backgroundColor = "grey";
+	document.getElementById("Operations-button").style.backgroundColor = "white";
+
 	tableOperations.style.display="none";
 	tableAllotBatch.style.display="block";
 	tableBatchInfo.style.display="none";
 }
 
-function loadBatchInfoTable(){
-	alert("Batch Info Pressed");	
+function loadOperationsTable(){
+	// alert("Operations Pressed");	
 	
-	tableOperations.style.display="none";
+	document.getElementById("BatchInfo-button").style.backgroundColor = "white";
+	document.getElementById("AllotBatch-button").style.backgroundColor = "white";
+	document.getElementById("Operations-button").style.backgroundColor = "grey";
+
+	tableOperations.style.display="block";
 	tableAllotBatch.style.display="none";
-	tableBatchInfo.style.display="block";
+	tableBatchInfo.style.display="none";
 }
+
+init();
